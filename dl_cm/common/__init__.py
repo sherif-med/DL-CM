3e4db783a2f394c3c10bf9db5a86c5292fb05411
sherif-med <cherif.abedrazek@gmail.com>
2024-02-13 01:30:57 +0100

    exporting all regstries at common module level

---
+++ dl_cm/common/__init__.py
@@ -0,0 +1,10 @@
+
+from .datasets.augmented_dataset import AUGMENTATION_TRANSFORMATION_REGISTERY
+from .datasets import DATASETS_REGISTERY
+from .datasets.preprocessed_dataset import PREPROCESSING_REGISTERY
+
+from .trainers.callbacks import CALLBACKS_REGISTERY
+from .trainers.loggers import LOGGERS_REGISTERY
+
+from .tasks import TASKS_REGISTERY
+from .tasks.optimizer import OPTIMIZER_REGiSTERY
