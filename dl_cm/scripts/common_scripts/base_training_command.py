761cca6d6c43b8aee916d2342c73d803b528e88c
sherif-med <cherif.abedrazek@gmail.com>
2024-02-13 01:32:47 +0100

    base training command doesn't need to have device option

--- dl_cm/scripts/common_scripts/base_training_command.py
+++ dl_cm/scripts/common_scripts/base_training_command.py
@@ -1,9 +1,9 @@
 import click
 from . import chain_decorators, config_file_option, pytorch_accelerator_option
 
-BaseTrainingCommand = chain_decorators(click.command(), config_file_option(), pytorch_accelerator_option())
+BaseTrainingCommand = chain_decorators(click.command(), config_file_option())
 
-def open_config_file(conf_file_path):
+def open_config_file(conf_file_path: str) -> dict :
     import yaml
     with open(conf_file_path, "r") as f:
         config = yaml.safe_load(f)
@@ -12,8 +12,8 @@ def open_config_file(conf_file_path):
 if __name__ == "__main__":
     
     @BaseTrainingCommand
-    def main(config_path, device):        
-        print(f"{config_path=} {device=}")
+    def main(config_path):        
+        print(f"{config_path=}")
         if config_path:
             config = open_config_file(config_path)
             print(config)    
