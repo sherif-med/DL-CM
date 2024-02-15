b74e8dbec7928b783449f83caebb9a6dc1ff8239
sherif-med <cherif.abedrazek@gmail.com>
2024-02-15 15:34:01 +0100

    basetask initates critreon and train/valid loss metrics and train/valid metrics

--- dl_cm/common/tasks/base_task.py
+++ dl_cm/common/tasks/base_task.py
@@ -1,4 +1,6 @@
 import pytorch_lightning as pl
+from dl_cm.common.tasks.metrics import init_metric_collection_from_config
+from dl_cm.common.tasks.criterion import init_loss_metrics_dict_from_config, load_critireon_from_config
 
 class BaseTask(pl.LightningModule):
     def __init__(self, task_config:dict):
@@ -7,6 +9,11 @@ class BaseTask(pl.LightningModule):
         self.save_hyperparameters(task_config)
         # Set the task name to the name of the current class
         self.hparams["task_name"] = type(self).__name__
+        
+        self.criterion = load_critireon_from_config(self.task_config.get("critireon"))
+        self.train_loss_metrics, self.train_metrics_log_flags = init_loss_metrics_dict_from_config(self.task_config.get("critireon"))
+        self.valid_loss_metrics, self.valid_metrics_log_flags = init_loss_metrics_dict_from_config(self.task_config.get("critireon"))
+        self.train_metrics, self.valid_metrics = init_metric_collection_from_config(self.task_config.get("metrics"))        
         # Call the post initialization method
         self.post_init()
 
