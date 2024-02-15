6d611080a764580b72443b9ebd6d0ade4b8bfe1a
sherif-med <cherif.abedrazek@gmail.com>
2024-02-15 15:22:06 +0100

    fixing arguments name in partial datamodule

--- dl_cm/common/datasets/datamodule.py
+++ dl_cm/common/datasets/datamodule.py
@@ -4,12 +4,12 @@ from pytorch_lightning.utilities.types import TRAIN_DATALOADERS
 
 class PartialDataModule(pl.LightningDataModule):
     
-    def __init__(self, train_partial=None, val_parital=None, test_partial=None, prediction_partial=None):
+    def __init__(self, train=None, valid=None, test=None, prediction=None):
         super().__init__()
-        self.train_partial = train_partial
-        self.val_parital = val_parital
-        self.test_partial = test_partial
-        self.prediction_partial = prediction_partial
+        self.train_partial = train
+        self.val_parital = valid
+        self.test_partial = test
+        self.prediction_partial = prediction
     
     
     def train_dataloader(self):
