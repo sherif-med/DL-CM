6d38d8e33cd79a0576ea4d3269ba0b5726e83df6
sherif-med <cherif.abedrazek@gmail.com>
2024-02-15 15:18:51 +0100

    adding progressbar and ModelCheckpoint to callbacks to registry (needs to add all pl callbacks)

--- dl_cm/common/trainers/callbacks.py
+++ dl_cm/common/trainers/callbacks.py
@@ -1,3 +1,9 @@
 from dl_cm.utils.registery import Registry
 
 CALLBACKS_REGISTERY = Registry("Callbacks")
+
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+
+CALLBACKS_REGISTERY.register(TQDMProgressBar)
+CALLBACKS_REGISTERY.register(ModelCheckpoint)
