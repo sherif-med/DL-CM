8a6721f1e3e92e3e4467730a846825d8693c5c8a
sherif-med <cherif.abedrazek@gmail.com>
2024-02-15 15:17:54 +0100

    adding pl TensorBoardLogger to loggers registry

--- dl_cm/common/trainers/loggers.py
+++ dl_cm/common/trainers/loggers.py
@@ -1,3 +1,6 @@
 from dl_cm.utils.registery import Registry
 
 LOGGERS_REGISTERY = Registry("Loggers")
+
+from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
+LOGGERS_REGISTERY.register(TensorBoardLogger)
