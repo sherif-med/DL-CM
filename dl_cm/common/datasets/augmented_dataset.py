e10e0f058c07d81b358d8428d6bd9d396222225b
sherif-med <cherif.abedrazek@gmail.com>
2024-02-18 04:54:40 +0100

    adding identity transformation && using transClasses in augmented datset

--- dl_cm/common/datasets/augmented_dataset.py
+++ dl_cm/common/datasets/augmented_dataset.py
@@ -2,16 +2,27 @@ import copy
 from dl_cm.utils.registery import Registry
 from . import CompositionDataset
 from collections.abc import Iterable
+from dl_cm.common.datasets.transformations.general_transformation import GeneralRevrsibleTransformation
 
 AUGMENTATION_TRANSFORMATION_REGISTERY = Registry("Augmentation transformation")
-AUGMENTATION_TRANSFORMATION_REGISTERY.register(lambda x:x, "id")
+
+@AUGMENTATION_TRANSFORMATION_REGISTERY.register(name="id")
+class TransIdentity(GeneralRevrsibleTransformation):
+    def identity(x):
+        return x
+    
+    def __init__(self):        
+        GeneralRevrsibleTransformation.__init__(self,
+            fwdfn=TransIdentity.identity,
+            rwdfn=TransIdentity.identity,
+        )
 
 class AugmentedDataset(CompositionDataset):
     
     def __init__(self, parent_dataset, augmentations:Iterable=("id",)):
         CompositionDataset.__init__(self, parent_dataset)
         if isinstance(next(iter(augmentations)), str):
-            augmentations = [AUGMENTATION_TRANSFORMATION_REGISTERY.get(k) for k in augmentations]
+            augmentations = [AUGMENTATION_TRANSFORMATION_REGISTERY.get(k)() for k in augmentations]
         self.augmentations = augmentations
     
     def __len__(self):
