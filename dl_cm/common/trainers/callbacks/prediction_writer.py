ff1b184e51699fd91a9b7dee0f4e079477676084
sherif-med <cherif.abedrazek@gmail.com>
2024-02-21 17:17:51 +0100

    ImagesPredictionWriter get predicted map key as prediction step returns a dict of output

--- dl_cm/common/trainers/callbacks/prediction_writer.py
+++ dl_cm/common/trainers/callbacks/prediction_writer.py
@@ -12,10 +12,12 @@ class ImagesPredictionWriter(BasePredictionWriter):
         self,
         output_dir,
         write_interval,
+        predicted_map_key
     ):
         super().__init__(write_interval)
         self.output_dir = output_dir
         Path(self.output_dir).mkdir(exist_ok=True, parents=True)
+        self.predicted_map_key = predicted_map_key
 
     def write_on_batch_end(
         self,
@@ -27,12 +29,11 @@ class ImagesPredictionWriter(BasePredictionWriter):
         batch_idx,
         dataloader_idx,
     ):
-        preds, filenames = prediction["preds"], prediction["id"]
+        preds, filenames = prediction["preds"][self.predicted_map_key], batch["id"]
         preds = preds.cpu().numpy().astype('uint8')
 
         for prediction, filename in zip(preds, filenames):
             output_file = Path(self.output_dir, os.path.basename(filename)).with_suffix(".png")
-            #imsave(output_file, np.transpose(prediction, (1, 2, 0)))
             imsave(output_file, prediction[0], check_contrast=False)
     
     def on_predict_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
@@ -43,3 +44,4 @@ class ImagesPredictionWriter(BasePredictionWriter):
         self.write_on_batch_end(
             trainer, pl_module, outputs, batch_indices, batch, batch_idx, dataloader_idx
             )
+        
