53cf87ee6e328190b0fa4fefb97ed900a7d5dd81
sherif-med <cherif.abedrazek@gmail.com>
2024-03-06 02:05:47 +0100

    moving subdataset class def out of split function def && remving dataset shuffeling

--- dl_cm/common/datasets/split_datasets.py
+++ dl_cm/common/datasets/split_datasets.py
@@ -3,6 +3,24 @@
 import numpy as np
 from . import CompositionDataset
 
+# Define a subdataset class that uses these indices
+class SubDataset(CompositionDataset):
+    def __init__(self, parent, indices):
+        super().__init__(parent, copy_parent=False)
+        self.indices = indices
+    
+    def __len__(self):
+        return len(self.indices)
+    
+    def parent_index(self, index):
+        return self.indices[index]
+    
+    def __getitem__(self, idx):
+        if idx < 0 or idx >= len(self):
+            raise IndexError("Index out of range")
+        real_idx = self.indices[idx]
+        return self.parent_dataset.__getitem__(real_idx)
+
 def split_subdatasets_random(parent_dataset, fractions):
     """
     Split a parent dataset into non-overlapping subdatasets with random indices,
@@ -18,7 +36,7 @@ def split_subdatasets_random(parent_dataset, fractions):
     assert sum(fractions) <= 1, "The sum of the fractions must not exceed 1."
 
     total_length = len(parent_dataset)
-    shuffled_indices = np.random.permutation(total_length)
+    indices = np.arange(0, total_length, 1)
     
     # Calculate the number of elements for each fraction
     counts = [int(frac * total_length) for frac in fractions]
@@ -30,22 +48,7 @@ def split_subdatasets_random(parent_dataset, fractions):
     subdatasets = []
     for start, end in zip(starts, ends):
         # Extract the indices for this subdataset
-        sub_indices = shuffled_indices[start:end]
-        
-        # Define a subdataset class that uses these indices
-        class SubDataset(CompositionDataset):
-            def __init__(self, parent, indices):
-                super().__init__(parent, copy_parent=False)
-                self.indices = indices
-            
-            def __len__(self):
-                return len(self.indices)
-            
-            def __getitem__(self, idx):
-                if idx < 0 or idx >= len(self):
-                    raise IndexError("Index out of range")
-                real_idx = self.indices[idx]
-                return self.parent_dataset.__getitem__(real_idx)
+        sub_indices = indices[start:end]
         
         # Create a new subdataset instance for the current subset of indices
         subdatasets.append(SubDataset(parent_dataset, sub_indices))
