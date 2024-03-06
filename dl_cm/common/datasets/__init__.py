1f1310a94d537cb447e582863e33ca4999779c32
sherif-med <cherif.abedrazek@gmail.com>
2024-03-06 02:07:40 +0100

    adding baseDataset && dataset factory

--- dl_cm/common/datasets/__init__.py
+++ dl_cm/common/datasets/__init__.py
@@ -1,7 +1,29 @@
 from dl_cm.utils.registery import Registry
+from dl_cm.config_loaders import load_named_entity
+from dl_cm.utils.exceptions import OutOfTypesException
 import copy
 
-class CompositionDataset(object):
+DATASETS_REGISTERY = Registry("Datasets")
+
+class BaseDataset:
+    pass
+
+class DatasetFactory:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def create(cls, c_dataset):
+        if isinstance(c_dataset, str):
+            dataset_class = DATASETS_REGISTERY.get(c_dataset)
+            return dataset_class()
+        elif isinstance(c_dataset, dict):
+            return load_named_entity(DATASETS_REGISTERY, c_dataset)
+        elif isinstance(c_dataset, BaseDataset):
+            return c_dataset
+        else:
+            raise OutOfTypesException(c_dataset, (str, dict, BaseDataset,))
+
 
     def __init__(self, parent_dataset, copy_parent=True):
         self.parent_dataset = copy.copy(parent_dataset) if copy_parent else parent_dataset
