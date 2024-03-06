ba419c1223f902782c24e235538f999b45fc4191
sherif-med <cherif.abedrazek@gmail.com>
2024-03-06 02:16:27 +0100

    preprocessed dataset uses factory to init preprocessing fn && inheriting from composition dataset

--- dl_cm/common/datasets/preprocessed_dataset.py
+++ dl_cm/common/datasets/preprocessed_dataset.py
@@ -2,11 +2,30 @@
 from dl_cm.utils.registery import Registry
 from typing import Union, Callable
 from dl_cm.utils.exceptions import OutOfTypesException
+from dl_cm.common.datasets import DATASETS_REGISTERY
+from dl_cm.common.datasets import CompositionDataset
 
 PREPROCESSING_REGISTERY = Registry("Preprocessing")
 
 class PreprocessingBase:
     pass
+
+class PreprocessingFactory:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def create(cls, c_preprocessing):
+        if isinstance(c_preprocessing, str):
+            preprocessing_class = PREPROCESSING_REGISTERY.get(c_preprocessing)
+            return preprocessing_class()
+        elif isinstance(c_preprocessing, dict):
+            return load_named_entity(PREPROCESSING_REGISTERY, c_preprocessing)
+        elif isinstance(c_preprocessing, PreprocessingBase):
+            return c_preprocessing
+        else:
+            raise OutOfTypesException(c_preprocessing, (str, dict, PreprocessingBase,))
+
 
 @PREPROCESSING_REGISTERY.register(name="id")
 class PreprocessingId(PreprocessingBase):
@@ -15,18 +34,12 @@ class PreprocessingId(PreprocessingBase):
     def __call__(self, item):
         return item
 
-
-class PreprocessedDataset:
+@DATASETS_REGISTERY.register()
+class PreprocessedDataset(CompositionDataset):
     
-    def __init__(self, parent_dataset, preprocessing_fn: Union[str, PreprocessingBase] = "id"):
-        super().__init__()
-        self.parent_dataset = parent_dataset
-        if isinstance(preprocessing_fn, str):
-            preprocessing_fn = PREPROCESSING_REGISTERY.get(preprocessing_fn)
-        elif isinstance(preprocessing_fn, PreprocessingBase):
-            self.preprocessing_callable = preprocessing_fn
-        else:
-            raise OutOfTypesException(preprocessing_fn, (str, PreprocessingBase))
+    def __init__(self, parent_dataset, preprocessing_fn: Union[str, dict, PreprocessingBase] = "id"):
+        super().__init__(parent_dataset)
+        self.preprocessing_callable = PreprocessingFactory.create(preprocessing_fn)
     
     def __len__(self):
         return len(self.parent_dataset)
