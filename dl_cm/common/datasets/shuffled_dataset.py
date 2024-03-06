6bcd462a68a0763630eb360f7e4d8550a424d6a3
sherif-med <cherif.abedrazek@gmail.com>
2024-03-06 02:16:45 +0100

    shuffled dataset impl

---
+++ dl_cm/common/datasets/shuffled_dataset.py
@@ -0,0 +1,23 @@
+from typing import List, Any, Callable, Union
+from dl_cm.common.datasets import CompositionDataset
+from dl_cm.common.datasets import BaseDataset
+import numpy as np
+
+class ShuffledDataset(CompositionDataset):
+    
+    def __init__(self, parent_dataset:BaseDataset, shuffled_indices:Union[list, np.ndarray] = None):
+        super().__init__(parent_dataset, copy_parent=False)
+        if shuffled_indices:
+            assert len(self.parent_dataset) == len(shuffled_indices)
+            self.shuffled_indices = shuffled_indices
+        else:
+            self.shuffled_indices = np.random.permutation(len(self.parent_dataset))
+    
+    def parent_index(self, index):
+        return self.shuffled_indices[index]
+        
+    def __len__(self):
+        return len(self.parent_dataset)
+    
+    def __getitem__(self, index):
+        return self.parent_dataset[self.shuffled_indices[index]]
