3f1e86e54cab49c01287831e6387ba12603ef2ff
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-04 03:40:39 +0100

    adding CritireonFactory && removing critireon_from_config and loss_metrics_dict_from_config && adding new registry including combinedLoss

--- dl_cm/common/tasks/criterion.py
+++ dl_cm/common/tasks/criterion.py
@@ -2,6 +2,7 @@ from typing import Tuple
 from dl_cm.utils.registery import Registry
 from torchmetrics import MeanMetric
 import torch.nn as nn
+from dl_cm.utils.ppattern.factory import BaseFactory
 class CombinedLoss(BaseLoss):
     def __init__(self, losses: list[str, dict, BaseLoss], weights: list[float]=None):        
         """
@@ -41,52 +42,18 @@ class CombinedLoss(BaseLoss):
             total_loss += c_loss_value * weight
         losses_dict[self.__class__.__name__] = total_loss
         return losses_dict
-
-def load_critireon_from_config(critireon_config)->CombinedLoss:
-    
-    critireon_def = critireon_config.get("def")    
-    sub_losses = []
-    sub_weight = []    
-    for c_loss_name, c_loss_def in critireon_def.items():
-        sub_losses.append(CRITIREON_REGISTRY.get(c_loss_name)(**c_loss_def.get("params")))
-        sub_weight.append(c_loss_def.get("weight"))
-    
-    return CombinedLoss(sub_losses, sub_weight)
-
-def init_loss_metrics_dict_from_config(critireon_config)->Tuple[nn.ModuleDict, dict]:
-    """
-    Returns a tuple composed of 1) ModuleDict for loss metric tracking 2) dict for logging behavior of loss metrics
-    """
-    
-    total_log_on_epoch = critireon_config.get("log_on_epoch")
-    total_log_on_step = critireon_config.get("log_on_step")
-    
-    metrics_log_flags = {
-        "total_loss":{
-            "log_on_epoch" : total_log_on_epoch,
-            "log_on_step" : total_log_on_step
-        }
-    }
-    
-    metrics_elements = {
-        "total_loss": MeanMetric()
-    }
-    
-    critireon_def = critireon_config.get("def") 
-    
-    for c_loss_name, c_loss_def in critireon_def.items():
-        metrics_log_flags[c_loss_name] = {
-            "log_on_epoch":c_loss_def.get("log_on_epoch", True),
-            "log_on_step":c_loss_def.get("log_on_step", False)
-        }
-        metrics_elements[c_loss_name] = MeanMetric()
-    
-    loss_metrics = nn.ModuleDict(metrics_elements)
-    return loss_metrics, metrics_log_flags
-        
-    
-
 CRITIREON_REGISTRY = Registry("Critireon")
+CRITIREON_REGISTRY.register(CombinedLoss)
+
+class CritireonFactory(BaseFactory):
+    
+    @classmethod
+    def base_class(cls)-> type:
+        return BaseLoss
+    
+    @classmethod
+    def registry(cls) -> Registry:
+        return CRITIREON_REGISTRY
 
 for name in dir(nn.modules.loss):
     attr = getattr(nn.modules.loss, name)
@@ -95,4 +62,3 @@ for name in dir(nn.modules.loss):
 
 from segmentation_models_pytorch.losses import DiceLoss
 CRITIREON_REGISTRY.register(DiceLoss)
-
