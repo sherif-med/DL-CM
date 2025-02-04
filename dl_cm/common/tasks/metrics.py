c762664f47064edfa45c6f224468812bee7c6854
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-04 03:31:09 +0100

    adapting metric module to use factory (added basemetric) && remove old init_metric from config && update schema for new metric def

--- dl_cm/common/tasks/metrics.py
+++ dl_cm/common/tasks/metrics.py
@@ -1,30 +1,27 @@
 from typing import List, Tuple
 from dl_cm.utils.registery import Registry
-from torchmetrics import MetricCollection
 import torchmetrics
+from dl_cm.utils.ppattern.factory import BaseFactory
 
 METRICS_REGISTRY = Registry("Metrics")
 
+class BaseMetric(torchmetrics.metric.Metric):
+    pass
+
+class MetricsFactory(BaseFactory):
+
+    @classmethod
+    def base_class(cls)-> type:
+        return BaseMetric
+    
+    @classmethod
+    def registry(cls) -> Registry:
+        return METRICS_REGISTRY
+
+# Register all torchmetrics metrics
 for name in dir(torchmetrics):
     attr = getattr(torchmetrics, name)
     if isinstance(attr, type) and issubclass(attr, torchmetrics.metric.Metric):
         METRICS_REGISTRY.register(attr)
 
-def init_metric_collection_from_config(metrics_config)->Tuple[MetricCollection]:
-    """"""
-    
-    def init_mode_config(metrics_defs: List[dict], **kwargs)->MetricCollection:
-        """"""
-        metrics = []
-        for c_metric_def in metrics_defs:
-            c_metric_cls = METRICS_REGISTRY.get(c_metric_def.get("name"))
-            metrics.append(
-                c_metric_cls( **c_metric_def.get("params") )
-            )
-        metric_collection = MetricCollection(metrics, **kwargs)
-        return metric_collection
-    
-    train_mode_config = metrics_config.get("train_mode")
-    valid_mode_config = metrics_config.get("valid_mode")
-    
-    return init_mode_config(train_mode_config.get("defs"), prefix="train_"), init_mode_config(valid_mode_config.get("defs"), prefix="valid_")
+
