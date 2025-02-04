a2ab42f26fa852b4330e6d3c63b1d6fadcda7e32
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-04 03:26:04 +0100

    cretaing a abstract factory class as seperate module

---
+++ dl_cm/utils/ppattern/factory.py
@@ -0,0 +1,55 @@
+from dl_cm.utils.registery import Registry
+from dl_cm.config_loaders import load_named_entity
+from dl_cm.utils.exceptions import OutOfTypesException
+import collections
+from typing import Self
+from typing import TypeVar, Type
+
+BaseClass = TypeVar('BaseClass', bound=type)
+
+class BaseFactory:
+
+    @classmethod
+    def registry(cls) -> Registry:
+        raise NotImplementedError
+    
+    @classmethod
+    def base_class(cls) -> Type[BaseClass]:
+        raise NotImplementedError
+
+    @classmethod
+    @classmethod
+    def create(cls, param: str | dict | Type[BaseClass] | collections.abc.Iterable[Type[BaseClass]]) -> BaseClass | list[BaseClass]:
+        """
+        Create an instance of the class, or a list of instances from the parameters.
+
+        Parameters
+        ----------
+        param : str, dict, cls.base_class(), collections.abc.Iterable
+            Parameters to create the instance. If str, it is the name of the class to create.
+            If dict, it is the configuration for the class to create.
+            If cls.base_class(), it is the instance to return.
+            If collections.abc.Iterable, it is a list of parameters to create a list of instances.
+
+        Returns
+        -------
+        Self
+            An instance of the class, or a list of instances.
+
+        Raises
+        ------
+        OutOfTypesException
+            If the param is not of the expected types.
+        """
+
+        if isinstance(param, str):
+            dataset_class = cls.registry().get(param)
+            return dataset_class()
+        elif isinstance(param, dict):
+            return load_named_entity(cls.registry(), param)
+        elif isinstance(param, cls.base_class()):
+            return param
+        elif isinstance(param, collections.abc.Iterable):
+            return type(param)(cls.create(p) for p in param)
+        else:
+            raise OutOfTypesException(param, (str, dict, cls.base_class(),))
