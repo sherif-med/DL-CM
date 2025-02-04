27b4b24c80f97b9f367c45ecdef9f6c5be9ab360
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-04 03:32:08 +0100

    adding specific metric (for complex metric measurment) not used yet

--- dl_cm/common/tasks/metrics.py
+++ dl_cm/common/tasks/metrics.py
@@ -1,4 +1,3 @@
-from typing import List, Tuple
 from dl_cm.utils.registery import Registry
 import torchmetrics
 from dl_cm.utils.ppattern.factory import BaseFactory
@@ -7,6 +6,18 @@ METRICS_REGISTRY = Registry("Metrics")
 
 class BaseMetric(torchmetrics.metric.Metric):
     pass
+
+class specificMetric(BaseMetric):
+    
+    def __init__(self, *args, **kwargs):
+        self.predicted_map_key = kwargs.pop("predicted_map_key", None)
+        self.target_map_key = kwargs.pop("target_map_key", None)
+        super().__init__(*args, **kwargs)
+
+    def update(self, preds, target):
+        preds = preds.get(self.predicted_map_key) if self.predicted_map_key else preds
+        target = preds.get(self.target_map_key) if self.target_map_key else target
+        super().update(preds, target)
 
 class MetricsFactory(BaseFactory):
 
