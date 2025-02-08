1f030a65fd797472e3b21a53a1dffd0fcd8e48f7
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-08 04:19:43 +0100

    impl UnsupervisedLearner step method

--- dl_cm/common/learning/unsupervised_learner.py
+++ dl_cm/common/learning/unsupervised_learner.py
@@ -1,7 +1,7 @@
 from dl_cm.common.learning.criterion_learner import CriterionLearner
 from dl_cm.utils.ppattern.data_validation import validationMixin
 import pydantic as pd
-from dl_cm.common.tasks.criterion import lossOutputStruct
+from dl_cm.common.typing import lossOutputStruct, StepInputStruct, StepOutputStruct
 
 class UnsupervisedLearner(CriterionLearner, validationMixin):
 
@@ -15,6 +15,15 @@ class UnsupervisedLearner(CriterionLearner, validationMixin):
         CriterionLearner.__init__(self, learner_config)
         self.predicted_key : str = learner_config.pop("predicted_key")
     
+    def step(self, batch: StepInputStruct, compute_loss=True) -> StepOutputStruct:
+        predictions = self.model()(batch["inputs"])
+        if compute_loss:
+            loss_dict = self.criteron_step(predictions)
+        else:
+            loss_dict = {}
+        output = {"predictions":predictions, "loss":loss_dict}
+        return output
+
     def criteron_step(self, predictions:dict) -> lossOutputStruct:
         predicted = predictions[self.predicted_key]
         loss = self.criterion(predicted)
