0f175382ae97a738ef3fe1406907940746aea238
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-08 04:12:36 +0100

    BaseTask is a DLCM && removing uncessary info && updating step(IO) structs to have optional fields

--- dl_cm/common/tasks/base_task.py
+++ dl_cm/common/tasks/base_task.py
@@ -1,46 +1,41 @@
 import pytorch_lightning as pl
 from dl_cm.common.tasks.optimizer import load_optimizer_from_config, load_lr_scheduler_from_config
 import torch
-
+from dl_cm.common import DLCM
+from dl_cm.common.tasks import TASKS_REGISTERY
 from typing import List, Dict
-from dl_cm.common.tasks.criterion import lossOutputStruct
+from dl_cm.common.typing import lossOutputStruct, Registry
 from dataclasses import dataclass
 
 @dataclass
 class StepOutputStruct:
     loss: lossOutputStruct | torch.Tensor
     predictions: Dict | torch.Tensor
-    target: Dict | torch.Tensor
-    input: Dict | torch.Tensor
-    metadata: Dict
-    auxiliary: Dict
+    target: Dict | torch.Tensor = None
+    inputs: Dict | torch.Tensor = None
+    metadata: Dict = None
+    auxiliary: Dict = None
 
 @dataclass
 class StepInputStruct:
     inputs: Dict | torch.Tensor
-    targets: Dict | torch.Tensor
-    metadata: Dict
-    auxiliary: Dict
+    targets: Dict | torch.Tensor = None
+    metadata: Dict = None
+    auxiliary: Dict = None
 
-class BaseTask(pl.LightningModule):
+class BaseTask(pl.LightningModule, DLCM):
+
+    @staticmethod
+    def registry() -> Registry:
+        return TASKS_REGISTERY
+
     def __init__(self, task_config:dict):
         super().__init__()
         self.task_config: dict = task_config
         self.save_hyperparameters(task_config)
         self.hparams["task_name"] = type(self).__name__
         self.lr = task_config.get("optimizer").get("params").get("lr")
-        
-    def info(self):
-        """
-        Returns the task description defined in the child class.
-        """
-        # Check if the child class has a DESCRIPTION attribute
-        if hasattr(self, 'DESCRIPTION'):
-            return self.DESCRIPTION
-        else:
-            # Return a default message or raise an error if DESCRIPTION is not defined
-            return "No description provided."
-            
+                
     def configure_optimizers(self):
         optimizer = load_optimizer_from_config(self.model.parameters(), self.task_config.get("optimizer"))
         lr_scheduler = load_lr_scheduler_from_config(optimizer, self.task_config.get("lr_scheduler")) if self.task_config.get("lr_scheduler") else None
