7d5436bfe7b157f5150f1a195c41186183e85647
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-09 03:47:06 +0100

    adapting loggers module to adhere to (base|factory) pattern && registering pl loggers

--- dl_cm/common/trainers/loggers.py
+++ dl_cm/common/trainers/loggers.py
@@ -1,6 +1,24 @@
 from dl_cm.utils.registery import Registry
+from dl_cm.common import DLCM
+from dl_cm.utils.ppattern.factory import BaseFactory
+from dl_cm.utils.ppattern.data_validation import validationMixin
 
 LOGGERS_REGISTERY = Registry("Loggers")
 
-from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
-LOGGERS_REGISTERY.register(TensorBoardLogger)
+class BaseLogger(DLCM, validationMixin):
+
+    @staticmethod
+    def registry() -> Registry:
+        return LOGGERS_REGISTERY
+    
+class LoggersFactory(BaseFactory):
+    
+    @staticmethod
+    def base_class()-> type:
+        return BaseLogger
+
+import pytorch_lightning.loggers as pl_loggers
+for name in dir(pl_loggers):
+    attr = getattr(pl_loggers, name)
+    if isinstance(attr, type) and issubclass(attr, pl_loggers.Logger) and attr.__module__ == pl_loggers.__name__:
+        LOGGERS_REGISTERY.register(attr)
