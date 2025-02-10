1b7b897f1f0594dc79f99eb6739a0b1bf2d5e15c
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-10 04:44:30 +0100

    updating steps dataclasses type annot && adding breif description to base task && adding config schema

--- dl_cm/common/tasks/base_task.py
+++ dl_cm/common/tasks/base_task.py
@@ -3,33 +3,48 @@ from dl_cm.common.tasks.optimizer import load_optimizer_from_config, load_lr_sch
 import torch
 from dl_cm.common import DLCM
 from dl_cm.common.tasks import TASKS_REGISTERY
-from typing import List, Dict
+from typing import Dict
 from dl_cm.common.typing import lossOutputStruct, Registry
 from dataclasses import dataclass
+import pydantic as pd
+from dl_cm.common.typing import namedEntitySchema, Any
+from dl_cm.utils.ppattern.data_validation import validationMixin
 
 @dataclass
 class StepOutputStruct:
     loss: lossOutputStruct | torch.Tensor
-    predictions: Dict | torch.Tensor
-    target: Dict | torch.Tensor = None
+    predictions: Dict[str, Any] | torch.Tensor
+    targets: Dict[str, Any] | torch.Tensor = None
     inputs: Dict | torch.Tensor = None
     metadata: Dict = None
     auxiliary: Dict = None
 
 @dataclass
 class StepInputStruct:
-    inputs: Dict | torch.Tensor
-    targets: Dict | torch.Tensor = None
+    inputs: Dict[str, Any] | torch.Tensor
+    targets: Dict[str, Any] | torch.Tensor = None
     metadata: Dict = None
     auxiliary: Dict = None
 
-class BaseTask(pl.LightningModule, DLCM):
+class BaseTask(pl.LightningModule, DLCM, validationMixin):
+    """
+    Base class for all tasks in the DLCM framework.
+    A task is a self-contained unit of work that encapsulates a specific learning problem.
+    It is responsible for defining the models, optimizers, and learning logic for a particular problem through learner class.
+    """
 
     @staticmethod
     def registry() -> Registry:
         return TASKS_REGISTERY
 
+    @staticmethod
+    def config_schema()-> pd.BaseModel:
+        class ValidConfig(pd.BaseModel):
+            learner: namedEntitySchema = None
+        return ValidConfig
+
     def __init__(self, task_config:dict):
+        validationMixin.__init__(self, task_config)
         super().__init__()
         self.task_config: dict = task_config
         self.save_hyperparameters(task_config)
