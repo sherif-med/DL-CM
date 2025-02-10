b4babc234116f6d23da948f479ce9a99929a278f
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-10 04:39:17 +0100

    adding decorate metric to adapt external metrics like torch metrics to support key specific update

--- dl_cm/common/tasks/metrics.py
+++ dl_cm/common/tasks/metrics.py
@@ -9,6 +9,27 @@ from dl_cm.common.typing import namedEntitySchema
 
 METRICS_REGISTRY = Registry("Metrics")
 
+def decorate_metric(metric_cls: type[Metric]):
+    """Decorator to make any torchmetrics.Metric work with specific dictionary inputs keys"""
+    class WrappedMetric(metric_cls, BaseMetric):
+        def __init__(self, preds_key: str = None, target_key: str = None, *args, **kwargs):
+            """Wraps a metric to extract values from dictionary inputs."""
+            super().__init__(*args, **kwargs)
+            BaseMetric.__init__(self)
+            self.preds_key = preds_key
+            self.target_key = target_key
+
+        def update(self, preds: dict, target: dict=None):
+            """Extract tensors and call the original update method."""
+            pred_tensor = preds[self.preds_key] if self.preds_key else preds
+            if target is None:
+                super().update(pred_tensor)
+                return
+            target_tensor = target[self.target_key] if self.target_key else target
+            super().update(pred_tensor, target_tensor)
+
+    return WrappedMetric
+
 class BaseMetric(Metric, DLCM, validationMixin):
 
     @staticmethod
@@ -34,7 +55,7 @@ class MetricsFactory(BaseFactory):
 # Register all torchmetrics metrics
 for name in dir(torchmetrics):
     attr = getattr(torchmetrics, name)
-    if isinstance(attr, type) and issubclass(attr, torchmetrics.metric.Metric):
-        METRICS_REGISTRY.register(attr)
+    if isinstance(attr, type) and issubclass(attr, Metric):
+        METRICS_REGISTRY.register(obj=decorate_metric(attr), name=attr.__name__)
 
 
