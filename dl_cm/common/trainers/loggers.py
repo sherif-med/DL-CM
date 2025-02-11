1890fb0ff0a45beb2d4c83d9320924624346bed4
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-11 22:22:36 +0100

    adding schema validation for baseLogger

--- dl_cm/common/trainers/loggers.py
+++ dl_cm/common/trainers/loggers.py
@@ -2,14 +2,24 @@ from dl_cm.utils.registery import Registry
 from dl_cm.common import DLCM
 from dl_cm.utils.ppattern.factory import BaseFactory
 from dl_cm.utils.ppattern.data_validation import validationMixin
+import pydantic as pd
+from dl_cm.common.typing import namedEntitySchema
 
 LOGGERS_REGISTERY = Registry("Loggers")
 
 class BaseLogger(DLCM, validationMixin):
 
     @staticmethod
+    def config_schema() -> pd.BaseModel:
+        return namedEntitySchema
+    
+    @staticmethod
     def registry() -> Registry:
         return LOGGERS_REGISTERY
+    
+    def __init__(self, config: dict) -> None:
+        validationMixin.__init__(self, config)
+        super().__init__(**config)
     
 class LoggersFactory(BaseFactory):
     
