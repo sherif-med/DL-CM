92c5b6726642deadab73341d179bc2122de72aaf
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-11 22:20:42 +0100

    adding callback factory and schema for base callback class

--- dl_cm/common/trainers/callbacks/__init__.py
+++ dl_cm/common/trainers/callbacks/__init__.py
@@ -1,12 +1,29 @@
 from dl_cm.utils.registery import Registry
 from dl_cm.common import DLCM
+from dl_cm.utils.ppattern.factory import BaseFactory
+from dl_cm.common.typing import namedEntitySchema
+import pydantic as pd
+
 CALLBACKS_REGISTERY = Registry("Callbacks")
 
 class baseCallback(DLCM):
+
+    @staticmethod
+    def config_schema() -> pd.BaseModel:
+        return namedEntitySchema
     
     @staticmethod
     def registry() -> Registry:
         return CALLBACKS_REGISTERY
+    
+    def __init__(self, config: dict) -> None:
+        super().__init__(**config)
+
+class CallbacksFactory(BaseFactory):
+    
+    @staticmethod
+    def base_class()-> type:
+        return baseCallback
 
 from pytorch_lightning.callbacks.progress import TQDMProgressBar
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
