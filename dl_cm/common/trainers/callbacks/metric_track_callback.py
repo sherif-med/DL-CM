9fcf3f0a297b00bd2234b99ca4278bf60c0b2141
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-11 22:14:54 +0100

    updating metric track callback to update binary and loss metrics (loss metrics are initialized on_fit_start if respective learner is a Criterion learner && removing predicted and target key as it's base metric responsibility

--- dl_cm/common/trainers/callbacks/metric_track_callback.py
+++ dl_cm/common/trainers/callbacks/metric_track_callback.py
@@ -4,58 +4,111 @@ import pydantic as pd
 from typing import List
 from torchmetrics import MetricCollection
 from dl_cm.common.tasks.metrics import MetricsFactory
-from dl_cm.common.tasks.base_task import StepOutputDict
+from dl_cm.common.typing import StepOutputStruct, namedEntitySchema
+from dl_cm.common.tasks.criterion import BaseLoss
+from dl_cm.common.tasks.metrics import BaseMetric
+from dl_cm.common.tasks.base_task import BaseTask
+from dl_cm.common.learning.criterion_learner import CriterionLearner
 
 class metricTrackCallback(pl.Callback, validationMixin):
 
     @staticmethod
     def config_schema()-> pd.BaseModel:
         class ValidConfig(pd.BaseModel):
-            metrics: List[dict]
-            target_key: str
-            predicted_key: str
+            metrics: List[namedEntitySchema]
         return ValidConfig
+    
+    binary_train_metrics : MetricCollection
+    binary_valid_metrics : MetricCollection
+    loss_train_metrics : MetricCollection
+    loss_valid_metrics : MetricCollection
+    task_learner_criterion : BaseLoss
     
     def __init__(self, config: dict):
         validationMixin.__init__(self, config)
         pl.Callback.__init__(self)
-        self.target_key = config.get("target_key")
-        self.predicted_key = config.get("predicted_key")
-        self.train_metrics : MetricCollection
-        self.valid_metrics : MetricCollection
-        self.init_metrics()
+        loaded_metrics = MetricsFactory.create(config.get("metrics"))
+        self.extend_binary_metrics(loaded_metrics)
+    
+    def on_fit_start(self, trainer, pl_module: BaseTask):
+        if isinstance(pl_module.learner, CriterionLearner):
+            self.task_learner_criterion = pl_module.learner.criterion
+            self.extend_loss_metrics(pl_module.learner.criterion)
 
-    def init_metrics(self):
+    def extend_binary_metrics(self, metrics: BaseMetric | List[BaseMetric]):
         """
         Initializes the metrics collections for training and validation.
         This method sets up the general metrics for both training and validation phases.
-        """        
+        """
+        metrics = metrics if isinstance(metrics, list) else [metrics]
         # General metrics (train)
-        train_metrics = MetricCollection(MetricsFactory.create(self.task_config.get("metrics")), prefix="train_")
-        if self.train_metrics is None:
-            self.train_metrics = train_metrics
+        train_metrics = MetricCollection(metrics, prefix="train_")
+        if self.binary_train_metrics is None:
+            self.binary_train_metrics = train_metrics
         else:
-            self.train_metrics.add_metrics(train_metrics)
+            self.binary_train_metrics.add_metrics(train_metrics)
         
         # General metrics (valid)
         valid_metrics = train_metrics.clone(prefix="valid_")
-        if self.valid_metrics is None:
-            self.valid_metrics = valid_metrics
+        if self.binary_valid_metrics is None:
+            self.binary_valid_metrics = valid_metrics
         else:
-            self.valid_metrics.add_metrics(valid_metrics)
+            self.binary_valid_metrics.add_metrics(valid_metrics)
+    
+    def extend_loss_metrics(self, criterion:BaseLoss):
+        # Loss metrics initialization (train)
+        loss_train_metrics : MetricCollection = \
+              criterion.as_metric_collection().clone(prefix="train_")
+        if self.loss_train_metrics is None:
+            self.loss_train_metrics = loss_train_metrics
+        else:
+            self.loss_train_metrics.add_metrics(loss_train_metrics)
+        # Loss metrics initialization (valid)
+        loss_valid_metrics : MetricCollection = \
+            self.loss_train_metrics.clone(prefix="valid_")
+        if self.loss_valid_metrics is None:
+            self.loss_valid_metrics = loss_valid_metrics
+        else:
+            self.loss_valid_metrics.add_metrics(loss_valid_metrics)
     
     def on_train_batch_end(
-        self, trainer: pl.Trainer, pl_module: pl.LightningModule, outputs: StepOutputDict, batch, batch_idx: int
+        self, trainer: pl.Trainer, pl_module: pl.LightningModule, outputs: StepOutputStruct, batch, batch_idx: int
     ) -> None:
-        preds = outputs["prediction"][self.predicted_key]
-        targets = outputs["target"][self.target_key]
-        self.train_metrics.update(preds=preds, target=targets)
-        return
+        self.binary_train_metrics.update(preds=outputs.predictions, target=outputs.targets)
+        if isinstance(outputs.loss, dict):
+            for (loss_name, loss_value) in outputs.loss.items():
+                self.loss_train_metrics[loss_name].update(loss_value)
+        else:
+            self.loss_train_metrics[self.task_learner_criterion.name()].update(outputs.loss)
 
     def on_validation_batch_end(
-            self, trainer: pl.Trainer, pl_module: pl.LightningModule, outputs: StepOutputDict, batch, batch_idx: int
+            self,
+            trainer: pl.Trainer,
+            pl_module: pl.LightningModule,
+            outputs: StepOutputStruct,
+            batch,
+            batch_idx: int,
+            dataloader_idx: int = 0,
     ) -> None:
-        preds = outputs["prediction"][self.predicted_key]
-        targets = outputs["target"][self.target_key]
-        self.valid_metrics.update(preds=preds, target=targets)
-        return
+        self.binary_valid_metrics.update(preds=outputs.predictions, target=outputs.targets)
+        if isinstance(outputs.loss, dict):
+            for (loss_name, loss_value) in outputs.loss.items():
+                self.loss_valid_metrics[loss_name].update(loss_value)
+        else:
+            self.loss_valid_metrics[self.task_learner_criterion.name()].update(outputs.loss)
+    
+    def on_train_epoch_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule)-> None:
+        self.binary_train_metrics.compute()
+        self.loss_train_metrics.compute()
+    
+    def on_validation_epoch_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule)-> None:
+        self.binary_valid_metrics.compute()
+        self.loss_valid_metrics.compute()
+    
+    def on_train_epoch_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule)-> None:
+        self.binary_train_metrics.reset()
+        self.loss_train_metrics.reset()
+        
+    def on_validation_epoch_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule)-> None:
+        self.binary_valid_metrics.reset()
+        self.loss_valid_metrics.reset()
