4d0120bbb0e477698cc90c7139bfb828b7fa4eff
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-11 22:23:24 +0100

    adding plugins (base|factory) strategy classes

---
+++ dl_cm/common/trainers/plugins.py
@@ -0,0 +1,26 @@
+from dl_cm.utils.registery import Registry
+from dl_cm.utils.ppattern.data_validation import validationMixin
+import pytorch_lightning as pl
+import pydantic as pd
+from dl_cm.common.typing import namedEntitySchema
+from dl_cm.utils.ppattern.factory import BaseFactory
+
+PLUGINS_REGISTERY = Registry("Plugins")
+
+class BasePlugin(pl.TrainerPlugin):
+    @staticmethod
+    def registry() -> Registry:
+        return PLUGINS_REGISTERY
+
+    @staticmethod
+    def config_schema() -> pd.BaseModel:
+        return namedEntitySchema
+
+    def __init__(self, config: dict) -> None:
+        validationMixin.__init__(self, config)
+        super().__init__(**config)
+
+class PluginsFactory(BaseFactory):
+    @staticmethod
+    def base_class()-> type:
+        return BasePlugin
