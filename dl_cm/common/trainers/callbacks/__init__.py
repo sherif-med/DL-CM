cfed7603841b70835d754d72dd1234a1d618dac4
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-12 21:10:10 +0100

    adding validation for basecallback and dropping super init && using DLCM base_class_adapter for external callbacks while registering

--- dl_cm/common/trainers/callbacks/__init__.py
+++ dl_cm/common/trainers/callbacks/__init__.py
@@ -3,10 +3,11 @@ from dl_cm.common import DLCM
 from dl_cm.utils.ppattern.factory import BaseFactory
 from dl_cm.common.typing import namedEntitySchema
 import pydantic as pd
+from dl_cm.utils.ppattern.data_validation import validationMixin
 
 CALLBACKS_REGISTERY = Registry("Callbacks")
 
-class baseCallback(DLCM):
+class baseCallback(DLCM, validationMixin):
 
     @staticmethod
     def config_schema() -> pd.BaseModel:
@@ -17,7 +18,7 @@ class baseCallback(DLCM):
         return CALLBACKS_REGISTERY
     
     def __init__(self, config: dict) -> None:
-        super().__init__(**config)
+        validationMixin.__init__(self, config)
 
 class CallbacksFactory(BaseFactory):
     
@@ -30,7 +31,9 @@ from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 from pytorch_lightning.callbacks import LearningRateFinder
 from pytorch_lightning.callbacks import LearningRateMonitor
 
-CALLBACKS_REGISTERY.register(TQDMProgressBar)
-CALLBACKS_REGISTERY.register(ModelCheckpoint)
-CALLBACKS_REGISTERY.register(LearningRateFinder)
-CALLBACKS_REGISTERY.register(LearningRateMonitor)
+from functools import partial
+base_callback_adapter = partial(DLCM.base_class_adapter, base_cls=baseCallback)
+CALLBACKS_REGISTERY.register(TQDMProgressBar, base_class_adapter=base_callback_adapter)
+CALLBACKS_REGISTERY.register(ModelCheckpoint, base_class_adapter=base_callback_adapter)
+CALLBACKS_REGISTERY.register(LearningRateFinder, base_class_adapter=base_callback_adapter)
+CALLBACKS_REGISTERY.register(LearningRateMonitor, base_class_adapter=base_callback_adapter)
