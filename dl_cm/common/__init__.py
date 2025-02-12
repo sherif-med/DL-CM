2ad3e5f861437e8c2c60bc1286aefb3edf2eb29e
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-12 21:07:12 +0100

    adding staticethod base_class adapter to DLCM to be used by registers to adapat external classes to the respective base class (ex: torchmetrics should inherit from BaseMetric)

--- dl_cm/common/__init__.py
+++ dl_cm/common/__init__.py
@@ -16,4 +16,12 @@ from .models import MODELS_REGISTERY
 from dl_cm.utils.ppattern.self_registered_mixin import selfRegisteredMixin
 
 class DLCM(selfRegisteredMixin):
-    pass
+
+    @staticmethod
+    def base_class_adapter(external_cls: type, base_cls: type)-> type:
+        """Decorator to make any external class inherit from BaseClass"""
+        class WrappedClass(external_cls, base_cls):
+            def __init__(self, config:dict):
+                base_cls.__init__(self, config)
+                external_cls.__init__(config.get("params", {}))
+        return WrappedClass
