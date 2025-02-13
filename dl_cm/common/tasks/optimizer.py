658f283068c1d4e959e562df77bfa4ded4ea7b78
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-14 00:42:32 +0100

    fixing registering of external optimizers and lr_schedulers

--- dl_cm/common/tasks/optimizer.py
+++ dl_cm/common/tasks/optimizer.py
@@ -29,14 +29,18 @@ class LrSchedulerFactory(BaseFactory):
     def base_class()-> type:
         return BaseLrScheduler
 
+from functools import partial
+base_optimizer_adapter = partial(DLCM.base_class_adapter, base_cls=BaseOptimizer)
+base_lr_scheduler_adapter = partial(DLCM.base_class_adapter, base_cls=BaseLrScheduler)
+
 import torch.optim as optim
 for name in dir(optim):
     attr = getattr(optim, name)
     if isinstance(attr, type) and issubclass(attr, optim.Optimizer) and attr.__module__ == optim.__name__:
-        OPTIMIZER_REGiSTERY.register(attr)
+        OPTIMIZER_REGiSTERY.register(attr, name=name, base_class_adapter=base_optimizer_adapter)
 
 import torch.optim.lr_scheduler as lr_scheduler
 for name in dir(lr_scheduler):
     attr = getattr(lr_scheduler, name)
     if isinstance(attr, type) and issubclass(attr, lr_scheduler._LRScheduler):
-        LR_SCHEDULER_REGiSTERY.register(attr)
+        LR_SCHEDULER_REGiSTERY.register(attr, name=name, base_class_adapter=base_lr_scheduler_adapter)
