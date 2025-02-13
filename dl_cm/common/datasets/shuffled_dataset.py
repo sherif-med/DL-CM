1e4b8723f183a21559110220f9b3da623c647534
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-13 01:54:53 +0100

    shuffldDataset has a validation (shuffled indices has a wider range of types) && updating init to accept config

--- dl_cm/common/datasets/shuffled_dataset.py
+++ dl_cm/common/datasets/shuffled_dataset.py
@@ -3,17 +3,32 @@ from dl_cm.common.datasets import CompositionDataset
 from dl_cm.common.datasets import BaseDataset
 import numpy as np
 from dl_cm.common.datasets import DATASETS_REGISTERY
+from dl_cm.utils.ppattern.data_validation import validationMixin
+import pydantic as pd
+from dl_cm.common.functions import FunctionsFactory
 
+class ShuffledDataset(CompositionDataset, validationMixin):
 
-class ShuffledDataset(CompositionDataset):
+    @staticmethod
+    def config_schema()-> pd.BaseModel:
+        class ValidConfig(pd.BaseModel):
+            shuffled_indices: list[int] | np.ndarray | Callable | str = None
+        return ValidConfig
     
-    def __init__(self, parent_dataset:BaseDataset, shuffled_indices:Union[list, np.ndarray] = None):
-        super().__init__(parent_dataset, copy_parent=False)
-        if shuffled_indices:
-            assert len(self.parent_dataset) == len(shuffled_indices)
-            self.shuffled_indices = shuffled_indices
+    def __init__(self, config: dict):
+        validationMixin.__init__(self, config)
+        super().__init__(config)
+        if isinstance(config.get("shuffled_indices"), (list, np.ndarray)):
+            shuffled_indices = np.array(config.get("shuffled_indices"))
+        elif callable(config.get("shuffled_indices")):
+            shuffled_indices = config.get("shuffled_indices")(len(self.parent_dataset))
+        elif isinstance(config.get("shuffled_indices"), str):
+            shuffled_indices = FunctionsFactory.create(config.get("shuffled_indices"))
         else:
-            self.shuffled_indices = np.random.permutation(len(self.parent_dataset))
+            shuffled_indices = np.random.permutation(len(self.parent_dataset))
+        if len(shuffled_indices) != len(self.parent_dataset):
+            raise Exception(f"Shuffled indices length {len(shuffled_indices)} is not equal to parent dataset length {len(self.parent_dataset)}")
+        self.shuffled_indices = shuffled_indices
     
     def parent_index(self, index):
         return self.shuffled_indices[index]
