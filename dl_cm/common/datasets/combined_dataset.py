74ffc7c6a4b86e800daf807ba1c3e77dd499f7f5
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-13 01:49:36 +0100

    combinedDataset has validation && init accepts config

--- dl_cm/common/datasets/combined_dataset.py
+++ dl_cm/common/datasets/combined_dataset.py
@@ -1,15 +1,23 @@
-from dl_cm.common.datasets import BaseDataset, DatasetFactory, DATASETS_REGISTERY
+from dl_cm.common.datasets import BaseDataset, DatasetFactory
 import bisect
 import itertools
-from collections.abc import Iterable
-from typing import Union
+import pydantic as pd
+from dl_cm.utils.ppattern.data_validation import validationMixin
+from dl_cm.common.typing import namedEntitySchema
 
 
-class CombinedDataset(BaseDataset):
+class CombinedDataset(BaseDataset, validationMixin):
+
+    @staticmethod
+    def config_schema()-> pd.BaseModel:
+        class ValidConfig(pd.BaseModel):
+            datasets: list[namedEntitySchema]
+        return ValidConfig
     
-    def __init__(self, datasets_def: Iterable[Union[BaseDataset, dict, str]]):
-        
-        self.datasets = [ DatasetFactory.create(c_dst_def) for c_dst_def in datasets_def ]
+    def __init__(self, config: dict):
+        validationMixin.__init__(self, config)
+        super().__init__(config)
+        self.datasets : list[BaseDataset] = DatasetFactory.create(config.get("datasets"))
         
         self.datasets_lengths = [len(d) for d in self.datasets]
         self.cumulative_lengths = [0] + list(itertools.accumulate(self.datasets_lengths))
