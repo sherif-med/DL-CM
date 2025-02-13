8f85b3a4a4e0d4e2830709618cb47139b7110a6a
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-13 01:51:07 +0100

    items dataset has validation and changing init to accept config && adding description to explain the purpose of such a basic class

--- dl_cm/common/datasets/items_dataset.py
+++ dl_cm/common/datasets/items_dataset.py
@@ -1,9 +1,30 @@
 from typing import List, Any
 from . import BaseDataset
+import pydantic as pd
+from dl_cm.utils.ppattern.data_validation import validationMixin
 
-class ItemsDataset(BaseDataset):
+class ItemsDataset(BaseDataset, validationMixin):
+    """
+    A dataset class that wraps a list of items, providing a standardized interface for working with in-memory datasets.
+
+    This class is intended to differentiate between datasets that have already been loaded into memory and those that are lazy-loaded, such as generators.
+
+    By using this class, you can ensure that your dataset is fully loaded and accessible, making it easier to work with libraries and frameworks that expect a fully-realized dataset.
+
+    Attributes:
+        items (List[Any]): The list of items in the dataset
+    """
+
+    @staticmethod
+    def config_schema()-> pd.BaseModel:
+        class ValidConfig(pd.BaseModel):
+            items: list[Any]
+        return ValidConfig
     
-    def __init__(self, items: List[Any]):
+    def __init__(self, config: dict):
+        validationMixin.__init__(self, config)
+        super().__init__(config)
+        items = config.get("items")
         self.items = items
     
     def __len__(self):
