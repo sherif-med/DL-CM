951718ded75783473a1aefa964b602bc524a8545
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-13 01:48:49 +0100

    augmented dataset has validation mixin and fixing init to accept config

--- dl_cm/common/datasets/augmented_dataset.py
+++ dl_cm/common/datasets/augmented_dataset.py
@@ -5,7 +5,9 @@ from collections.abc import Iterable
 from dl_cm.common.datasets.transformations.general_transformation import GeneralRevrsibleTransformation
 
 from .transformations import TRANSFORMATION_REGISTRY
-from .transformations.general_transformation import GeneralTransformationFactory
+from .transformations.general_transformation import GeneralTransformationFactory, GeneralTransformation
+import pydantic as pd
+from dl_cm.utils.ppattern.data_validation import validationMixin
 
 @TRANSFORMATION_REGISTRY.register(name="id")
 class TransIdentity(GeneralRevrsibleTransformation):
@@ -19,11 +21,19 @@ class TransIdentity(GeneralRevrsibleTransformation):
         )
 
 
-class AugmentedDataset(CompositionDataset):
+class AugmentedDataset(CompositionDataset, validationMixin):
+
+    @staticmethod
+    def config_schema()-> pd.BaseModel:
+        class ValidConfig(pd.BaseModel):
+            augmentations: list[str] = ["id"]
+        return ValidConfig
     
-    def __init__(self, parent_dataset, augmentations:Iterable=("id",)):
-        CompositionDataset.__init__(self, parent_dataset)
-        self.augmentations = [GeneralTransformationFactory.create(k) for k in augmentations]
+    def __init__(self, config: dict):
+        validationMixin.__init__(self, config)
+        super().__init__(config)
+        self.augmentations : list[GeneralTransformation] = \
+            GeneralTransformationFactory.create(config.get("augmentations"))
     
     def __len__(self):
         return len(self.parent_dataset) * len(self.augmentations)
