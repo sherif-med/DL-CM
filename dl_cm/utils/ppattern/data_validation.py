0a658db40138023c4eed9fbe829eecc1123dcb0b
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-15 05:06:50 +0100

    changing validationMixiin to be an ABC && config schma is an abstract method

--- dl_cm/utils/ppattern/data_validation.py
+++ dl_cm/utils/ppattern/data_validation.py
@@ -1,19 +1,23 @@
 from pydantic import BaseModel
 from dl_cm import _logger as logger
+from dl_cm.utils.ppattern.init_check_mixin import InitCheckMixin
+from abc import ABC, abstractmethod
 
-class validationMixin:
+class validationMixin(InitCheckMixin, ABC):
 
     def __init__(self, data):
+        InitCheckMixin.__init__(self)
         self.validate(data)
-    
+
     def validate(self, data)-> None:
         try:
-            parsed = self.config_schema()(data)
+            _ = type(self).config_schema()(data) # TODO this should be fixed by making config_schema a classmethod
         except ValueError as e:
             logger.error("Data validation failed for class %s", self.__class__.__name__)
             logger.error(e)
             exit(1)
-    
+
+    @abstractmethod
     @staticmethod
     def config_schema()-> BaseModel:
-        raise NotImplementedError
+        pass
