783a4a93f6290da504b19669295f017a6b69ad80
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-15 05:22:32 +0100

    removing uncessary imports from dlcm common module && passing "params" as kwargs to external class at init

--- dl_cm/common/__init__.py
+++ dl_cm/common/__init__.py
@@ -1,18 +1,3 @@
-
-from .datasets.transformations import TRANSFORMATION_REGISTRY
-from .datasets import DATASETS_REGISTERY
-from .datasets.preprocessed_dataset import PREPROCESSING_REGISTERY
-
-from .trainers.callbacks import CALLBACKS_REGISTERY
-from .trainers.loggers import LOGGERS_REGISTERY
-
-from .tasks import TASKS_REGISTERY
-from .tasks.optimizer import OPTIMIZER_REGiSTERY, LR_SCHEDULER_REGiSTERY
-from .tasks.criterion import CRITIREON_REGISTRY
-from .tasks.metrics import METRICS_REGISTRY
-
-from .models import MODELS_REGISTERY
-
 from dl_cm.utils.ppattern.self_registered_mixin import selfRegisteredMixin
 
 class DLCM(selfRegisteredMixin):
@@ -23,5 +8,5 @@ class DLCM(selfRegisteredMixin):
         class WrappedClass(external_cls, base_cls):
             def __init__(self, config:dict):
                 base_cls.__init__(self, config)
-                external_cls.__init__(config.get("params", {}))
+                external_cls.__init__(**config.get("params", {}))
         return WrappedClass
