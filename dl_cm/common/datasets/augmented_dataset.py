9f816c27e576bd66c074ed72c854050ca23d248b
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-18 19:11:33 +0100

    MAJOR: removing depr mixin && ruff formatting

--- dl_cm/common/datasets/augmented_dataset.py
+++ dl_cm/common/datasets/augmented_dataset.py
@@ -1,52 +1,50 @@
-import copy
-from dl_cm.common.datasets import DATASETS_REGISTERY
-from . import CompositionDataset
-from collections.abc import Iterable
-from dl_cm.common.datasets.transformations.general_transformation import GeneralRevrsibleTransformation
+from dl_cm.common.datasets import COMPOSED_DATASET_CLASS
+from dl_cm.common.datasets.transformations.general_transformation import (
+    GeneralRevrsibleTransformation,
+)
 
+from . import CompositionDataset
 from .transformations import TRANSFORMATION_REGISTRY
-from .transformations.general_transformation import GeneralTransformationFactory, GeneralTransformation
-import pydantic as pd
-from dl_cm.utils.ppattern.data_validation import validationMixin
+from .transformations.general_transformation import (
+    GeneralTransformation,
+    GeneralTransformationFactory,
+)
+
 
 @TRANSFORMATION_REGISTRY.register(name="id")
 class TransIdentity(GeneralRevrsibleTransformation):
+    @staticmethod
     def identity(x):
         return x
-    
-    def __init__(self):        
-        GeneralRevrsibleTransformation.__init__(self,
+
+    def __init__(self):
+        GeneralRevrsibleTransformation.__init__(
+            self,
             fwdfn=TransIdentity.identity,
             rwdfn=TransIdentity.identity,
         )
 
 
-class AugmentedDataset(CompositionDataset, validationMixin):
+class AugmentedDataset(CompositionDataset[COMPOSED_DATASET_CLASS]):
+    def __init__(
+        self, augmentations: list[str | GeneralTransformation], *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.augmentations: list[GeneralTransformation] = (
+            GeneralTransformationFactory.create(augmentations)
+        )
 
-    @staticmethod
-    def config_schema()-> pd.BaseModel:
-        class ValidConfig(pd.BaseModel):
-            augmentations: list[str] = ["id"]
-        return ValidConfig
-    
-    def __init__(self, config: dict):
-        validationMixin.__init__(self, config)
-        super().__init__(config)
-        self.augmentations : list[GeneralTransformation] = \
-            GeneralTransformationFactory.create(config.get("augmentations"))
-    
     def __len__(self):
         return len(self.parent_dataset) * len(self.augmentations)
-    
+
     def parent_index(self, index):
         parent_item_idx = index // (len(self.augmentations))
         return parent_item_idx
-    
-    def __getitem__(self, idx):        
+
+    def __getitem__(self, idx):
         parent_item_idx = self.parent_index(idx)
         c_augmentation_idx = idx % (len(self.augmentations))
         c_augmentation = self.augmentations[c_augmentation_idx]
-        
+
         parent_item = self.parent_dataset[parent_item_idx]
         return c_augmentation(parent_item)
-
