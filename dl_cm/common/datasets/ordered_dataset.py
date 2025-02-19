c15597fcd9df27657c31d3f2ed7b425ddad3c4a1
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-19 12:57:14 +0100

    MAJOR: removing depr validation and eplict argument && ruff format

--- dl_cm/common/datasets/ordered_dataset.py
+++ dl_cm/common/datasets/ordered_dataset.py
@@ -1,24 +1,17 @@
 from typing import Callable
-from dl_cm.common.datasets import CompositionDataset
-from dl_cm.common.datasets.filtered_dataset import ItemsDataset
-from dl_cm.utils.ppattern.data_validation import validationMixin
-import pydantic as pd
+
+from dl_cm.common.datasets import COMPOSED_DATASET_CLASS, CompositionDataset
 from dl_cm.common.functions import FunctionsFactory
 
-class OrderedItemsDataset(CompositionDataset, validationMixin):
 
-    @staticmethod
-    def config_schema()-> pd.BaseModel:
-        class ValidConfig(pd.BaseModel):
-            order_fn: str | Callable
-        return ValidConfig
-
-    def __init__(self, config:dict):
-        validationMixin.__init__(self, config)
-        super().__init__(config)
-        element_to_value = FunctionsFactory.create(config.get("order_fn"))
+class OrderedItemsDataset(CompositionDataset[COMPOSED_DATASET_CLASS]):
+    def __init__(self, order_fn: str | Callable, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        element_to_value = FunctionsFactory.create(order_fn)
         indices = list(range(len(self.parent_dataset)))
-        self.reordered_items_indices = sorted(indices, key=lambda index: element_to_value(self.parent_dataset[index]))
+        self.reordered_items_indices = sorted(
+            indices, key=lambda index: element_to_value(self.parent_dataset[index])
+        )
 
     def __len__(self):
         return len(self.parent_dataset)
