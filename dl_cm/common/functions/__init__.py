4b38633b36826cfdae68dad8fd1e7fc8b8c70959
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-19 13:06:18 +0100

    remove depr validation for learner && ruff format && type annot typo fix

--- dl_cm/common/functions/__init__.py
+++ dl_cm/common/functions/__init__.py
@@ -1,11 +1,12 @@
-from dl_cm.utils.registery import Registry
-from dl_cm.utils.ppattern.factory import BaseFactory
 from collections.abc import Callable
+
+from dl_cm.utils.ppattern.factory import BaseFactory
+from dl_cm.utils.registery import Registry
 
 FUNCTIONS_REGISTERY = Registry("Functions")
 
+
 class FunctionsFactory(BaseFactory[Callable]):
-    
     @staticmethod
-    def base_class()-> type[Callable]:
+    def base_class() -> Callable:
         return Callable
--- dl_cm/common/learning/base_learner.py
+++ dl_cm/common/learning/base_learner.py
@@ -1,29 +1,20 @@
-from dl_cm.common.typing import StepInputStruct, StepOutputStruct
-from dl_cm.utils.ppattern.data_validation import validationMixin
-from dl_cm.common.models import ModelsFactory, BaseModel
-from dl_cm.common.typing import namedEntitySchema
-import pydantic as pd
-from dl_cm.utils.registery import Registry
-from dl_cm.common import DLCM
-from dl_cm.common.learning import LEARNERS_REGISTERY
 import torch
 
-class BaseLearner(torch.nn.Module, validationMixin, DLCM):
+from dl_cm.common import DLCM
+from dl_cm.common.learning import LEARNERS_REGISTERY
+from dl_cm.common.models import BaseModel, ModelsFactory
+from dl_cm.common.typing import StepInputStruct, StepOutputStruct, namedEntitySchema
+from dl_cm.utils.registery import Registry
 
+
+class BaseLearner(torch.nn.Module, DLCM):
     @staticmethod
     def registry() -> Registry:
         return LEARNERS_REGISTERY
 
-    @staticmethod
-    def config_schema()-> pd.BaseModel:
-        class ValidConfig(namedEntitySchema):
-            model: namedEntitySchema = None
-        return ValidConfig
-
-    def __init__(self, config: dict) -> None:
-        validationMixin.__init__(self, config)
-        super().__init__()
-        self._model : BaseModel = ModelsFactory.create(config.get("model"))
+    def __init__(self, model: BaseModel | namedEntitySchema, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._model: BaseModel = ModelsFactory.create(model)
 
     @property
     def model(self):
