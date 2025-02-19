87e13cb5be5df2849d38563f8661761ef87d97b1
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-19 11:31:37 +0100

    MAJOR: remove depr validation and eplicit argument && is_image is a static method && adding new extension && ruff format

--- dl_cm/common/datasets/images_dataset.py
+++ dl_cm/common/datasets/images_dataset.py
@@ -1,78 +1,74 @@
-
+import collections
 import glob
 import os
-from dl_cm.common.datasets.filtered_dataset import ItemsDataset, FilteredItemsDataset
-from dl_cm.common.datasets import CompositionDataset
-from skimage.io import imread
+from functools import partial
+
 import torch
-import pydantic as pd
-from dl_cm.utils.ppattern.data_validation import validationMixin
+from pydantic import DirectoryPath, validate_call
+from skimage.io import imread
 
-class FilesWithinDirectoryDataset(ItemsDataset, validationMixin):
+from dl_cm.common.datasets import COMPOSED_DATASET_CLASS, CompositionDataset
+from dl_cm.common.datasets.filtered_dataset import FilteredItemsDataset
+from dl_cm.common.datasets.items_dataset import ItemsDataset
 
-    @staticmethod
-    def config_schema()-> pd.BaseModel:
-        class ValidConfig(pd.BaseModel):
-            directory_path: str
-        return ValidConfig
 
-    def __init__(self, config: dict):
-        validationMixin.__init__(self, config)
-        directory_path = config.get("directory_path")
+class FilesWithinDirectoryDataset(ItemsDataset):
+    @validate_call
+    def __init__(self, directory_path: DirectoryPath, *args, **kwargs):
         items_paths = glob.glob(os.path.join(directory_path, "*"))
-        config["items"] = items_paths
-        super().__init__(config)
+        super().__init__(items=items_paths, *args, **kwargs)
 
-DEFAULT_IMAGES_EXTENSIONS = tuple([".tif", ".jpeg", ".png"])
+
+DEFAULT_IMAGES_EXTENSIONS = tuple([".tif", ".jpeg", ".png", "jpg"])
 IMAGE_KEY = "image"
 
-class ImagesWithinDirectoryDataset(CompositionDataset, validationMixin):
+
+class ImagesWithinDirectoryDataset(CompositionDataset[COMPOSED_DATASET_CLASS]):
+    def __init__(
+        self,
+        directory_path: DirectoryPath,
+        image_extensions: collections.abc.Iterable[str] = DEFAULT_IMAGES_EXTENSIONS,
+        image_key: str = IMAGE_KEY,  # used as key for image tensor within getitem dict
+        *args,
+        **kwargs,
+    ):
+        self.image_key = image_key
+
+        parent_dataset = FilteredItemsDataset(
+            filter_fn=partial(
+                ImagesWithinDirectoryDataset.is_image_fp,
+                image_extensions=image_extensions,
+            ),
+            parent_dataset=FilesWithinDirectoryDataset(directory_path=directory_path),
+        )
+        super().__init__(parent_dataset=parent_dataset, *args, **kwargs)
 
     @staticmethod
-    def config_schema()-> pd.BaseModel:
-        class ValidConfig(pd.BaseModel):
-            directory_path: str
-            image_extensions: list[str] = DEFAULT_IMAGES_EXTENSIONS
-            image_key: str = IMAGE_KEY
-        return ValidConfig
-
-    def __init__(self, config: dict):
-        validationMixin.__init__(self, config)
-        super().__init__(config)
-        self.image_key : str = config.get("image_key")
-        def is_image_fp(filepath: str):
-            return filepath.lower().endswith(config.get("image_extensions", DEFAULT_IMAGES_EXTENSIONS))
-        root_directory_path = config.get("directory_path")
-        filterd_dataset_config = {
-            "filter_fn":is_image_fp,
-            "parent_dataset":FilesWithinDirectoryDataset(root_directory_path)
-        }
-        config["parent_dataset"] = FilteredItemsDataset(filterd_dataset_config)
-        config["copy_parent"] = False
-        super().__init__(config)
+    def is_image_fp(
+        filepath: str,
+        image_extensions: collections.abc.Iterable[str] = DEFAULT_IMAGES_EXTENSIONS,
+    ) -> bool:
+        return filepath.lower().endswith(image_extensions)
 
     def __len__(self):
         return len(self.parent_dataset)
 
-    def parent_index(self, index : int) -> int:
+    def parent_index(self, index: int) -> int:
         return index
 
     @staticmethod
     def read_image(fp):
         loaded_tensor = torch.from_numpy(imread(fp))
-        if len(loaded_tensor.shape)==2:
+        if len(loaded_tensor.shape) == 2:
             # if 2D image, add band dimension as last dimension
             loaded_tensor = loaded_tensor.unsqueeze(-1)
 
-        permutation_index = [i for i in range(2, len(loaded_tensor.shape))] + [0,  1]
+        permutation_index = [i for i in range(2, len(loaded_tensor.shape))] + [0, 1]
         # Apply the permutation to the tensor
         reshaped_tensor = loaded_tensor.permute(*permutation_index)
         return reshaped_tensor
 
     def __getitem__(self, index):
         item_fp = self.parent_dataset[index]
-        item = {
-            "id":item_fp,
-            self.image_key:self.read_image(item_fp).float()
-        }
+        item = {"id": item_fp, self.image_key: self.read_image(item_fp).float()}
         return item
