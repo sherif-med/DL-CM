7fcb2aa1892cec57cfae40bca0d8310a6898d6d0
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-21 08:32:53 +0100

    MAJOR: removing validation && ruff format

--- dl_cm/common/trainers/loggers.py
+++ dl_cm/common/trainers/loggers.py
@@ -1,36 +1,36 @@
-from dl_cm.utils.registery import Registry
 from dl_cm.common import DLCM
 from dl_cm.utils.ppattern.factory import BaseFactory
-from dl_cm.utils.ppattern.data_validation import validationMixin
-import pydantic as pd
-from dl_cm.common.typing import namedEntitySchema
+from dl_cm.utils.registery import Registry
 
 LOGGERS_REGISTERY = Registry("Loggers")
 
-class BaseLogger(DLCM, validationMixin):
 
-    @staticmethod
-    def config_schema() -> pd.BaseModel:
-        return namedEntitySchema
-    
+class BaseLogger(DLCM):
     @staticmethod
     def registry() -> Registry:
         return LOGGERS_REGISTERY
-    
-    def __init__(self, config: dict) -> None:
-        validationMixin.__init__(self, config)
-    
+
+    def __init__(self, *args, **config) -> None:
+        super().__init__(*args, **config)
+
+
 class LoggersFactory(BaseFactory[BaseLogger]):
-    
     @staticmethod
-    def base_class()-> type[BaseLogger]:
+    def base_class() -> type[BaseLogger]:
         return BaseLogger
 
+
 from functools import partial
+
 base_logger_adapter = partial(DLCM.base_class_adapter, base_cls=BaseLogger)
 
 import pytorch_lightning.loggers as pl_loggers
+
 for name in dir(pl_loggers):
     attr = getattr(pl_loggers, name)
-    if isinstance(attr, type) and issubclass(attr, pl_loggers.Logger) and attr.__module__ == pl_loggers.__name__:
+    if (
+        isinstance(attr, type)
+        and issubclass(attr, pl_loggers.Logger)
+        and attr.__module__ == pl_loggers.__name__
+    ):
         LOGGERS_REGISTERY.register(attr, base_class_adapter=base_logger_adapter)
