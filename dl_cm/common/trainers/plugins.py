8d7bb68e6fd276c3e8801549c98785e9c0fc2a96
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-21 08:38:55 +0100

    MAJOR: removing depr validation && registering pl plugins && ruff format

--- dl_cm/common/trainers/plugins.py
+++ dl_cm/common/trainers/plugins.py
@@ -1,26 +1,32 @@
-from dl_cm.utils.registery import Registry
-from dl_cm.utils.ppattern.data_validation import validationMixin
-import pytorch_lightning as pl
-import pydantic as pd
-from dl_cm.common.typing import namedEntitySchema
+from dl_cm.common import DLCM
 from dl_cm.utils.ppattern.factory import BaseFactory
+from dl_cm.utils.registery import Registry
 
 PLUGINS_REGISTERY = Registry("Plugins")
 
-class BasePlugin(pl.TrainerPlugin):
+
+class BasePlugin(DLCM):
     @staticmethod
     def registry() -> Registry:
         return PLUGINS_REGISTERY
 
-    @staticmethod
-    def config_schema() -> pd.BaseModel:
-        return namedEntitySchema
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
-    def __init__(self, config: dict) -> None:
-        validationMixin.__init__(self, config)
-        super().__init__(**config)
 
 class PluginsFactory(BaseFactory[BasePlugin]):
     @staticmethod
-    def base_class()-> type[BasePlugin]:
+    def base_class() -> type[BasePlugin]:
         return BasePlugin
+
+
+from functools import partial
+
+import pytorch_lightning.plugins as pl_plugins
+
+base_plugin_adapter = partial(DLCM.base_class_adapter, base_cls=BasePlugin)
+
+for name in dir(pl_plugins):
+    attr = getattr(pl_plugins, name)
+    if isinstance(attr, type) and attr.__module__ == pl_plugins.__name__:
+        PLUGINS_REGISTERY.register(attr, base_class_adapter=base_plugin_adapter)
