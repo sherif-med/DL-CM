98a61d3ae63dc00753e20e5d54ad4a34074fce77
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-21 08:30:21 +0100

    MAJOR removing depr validation && ruff format && trainer is a dlcm

--- dl_cm/common/trainers/base_trainer.py
+++ dl_cm/common/trainers/base_trainer.py
@@ -1,41 +1,43 @@
-from dl_cm.common.trainers.callbacks import CallbacksFactory
-from dl_cm.common.trainers.loggers import LoggersFactory
-from dl_cm.common.trainers.plugins import PluginsFactory
-from dl_cm.utils.registery import Registry
-from dl_cm.utils.ppattern.factory import BaseFactory
-from dl_cm.utils.ppattern.data_validation import validationMixin
 import pytorch_lightning as pl
-import pydantic as pd
+
+from dl_cm.common import DLCM
+from dl_cm.common.trainers.callbacks import CallbacksFactory, baseCallback
+from dl_cm.common.trainers.loggers import BaseLogger, LoggersFactory
+from dl_cm.common.trainers.plugins import BasePlugin, PluginsFactory
+from dl_cm.common.typing import namedEntitySchema
+from dl_cm.utils.ppattern.factory import BaseFactory
+from dl_cm.utils.registery import Registry
 
 TRAINERS_REGISTERY = Registry("Trainers")
 
-class BaseTrainer(pl.Trainer):
+
+class BaseTrainer(pl.Trainer, DLCM):
     @staticmethod
     def registry() -> Registry:
         return TRAINERS_REGISTERY
 
-    @staticmethod
-    def config_schema() -> pd.BaseModel:
-        class ValidConfig(pd.BaseModel):
-            params: dict = {}
-            callbacks: list[dict] = []
-            loggers: list[dict] = []
-            plugins: list[dict] = []
-        return ValidConfig
-
-    def __init__(self, config: dict) -> None:
-        validationMixin.__init__(self, config)
-        callbacks = CallbacksFactory.create(config.get("callbacks", []))
-        loggers = LoggersFactory.create(config.get("loggers", []))
-        plugins = PluginsFactory.create(config.get("plugins", []))
-        params: dict = config.get("params", {}) | {
+    def __init__(
+        self,
+        params: dict,
+        callbacks: list[namedEntitySchema | baseCallback] = [],
+        loggers: list[namedEntitySchema | BaseLogger] = [],
+        plugins: list[namedEntitySchema | BasePlugin] = [],
+        *args,
+        **kwargs,
+    ) -> None:
+        callbacks = CallbacksFactory.create(callbacks)
+        loggers = LoggersFactory.create(loggers)
+        plugins = PluginsFactory.create(plugins)
+        params: dict = params | {
             "callbacks": callbacks,
             "logger": loggers,
-            "plugins": plugins
+            "plugins": plugins,
         }
-        super().__init__(**params)
+        DLCM.__init__(self, *args, **kwargs)
+        pl.Trainer.__init__(self, **params)
+
 
 class TrainerFactory(BaseFactory[BaseTrainer]):
     @staticmethod
-    def base_class()-> type[BaseTrainer]:
+    def base_class() -> type[BaseTrainer]:
         return BaseTrainer
