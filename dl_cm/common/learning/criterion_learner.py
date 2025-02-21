0d8f7ea998e8b9ea6b145a4246c1095edf22f9a6
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-21 07:55:19 +0100

    MAJOR: removing depr validation and eplicit argument && ruuff format

--- dl_cm/common/learning/criterion_learner.py
+++ dl_cm/common/learning/criterion_learner.py
@@ -1,26 +1,15 @@
-from dl_cm.common.learning.base_learner import BaseLearner
-from dl_cm.utils.ppattern.data_validation import validationMixin
-from dl_cm.common.tasks.criterion import CritireonFactory
-import pydantic as pd
-from dl_cm.common.tasks.criterion import BaseLoss
-from dl_cm.common.typing import lossOutputStruct
-from dl_cm.common.learning.optimizable_learner import OptimizableLearner
 from abc import ABC, abstractmethod
 
-class CriterionLearner(OptimizableLearner, validationMixin, ABC):
+from dl_cm.common.learning.optimizable_learner import OptimizableLearner
+from dl_cm.common.tasks.criterion import BaseLoss, CritireonFactory
+from dl_cm.common.typing import lossOutputStruct, namedEntitySchema
 
-    @staticmethod
-    def config_schema()-> pd.BaseModel:
-        class ValidConfig(pd.BaseModel):
-            critireon: dict
-        return ValidConfig
 
-    def __init__(self, learner_config:dict):
-        validationMixin.__init__(self, learner_config)
-        super().__init__(self, learner_config)
-        self.criterion : BaseLoss = CritireonFactory.create(self.config.get("critireon"))
-    
+class CriterionLearner(OptimizableLearner, ABC):
+    def __init__(self, criterion: BaseLoss | namedEntitySchema, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.criterion: BaseLoss = CritireonFactory.create(criterion)
+
     @abstractmethod
     def criteron_step(self, *args, **kwargs) -> lossOutputStruct:
         pass
-    
