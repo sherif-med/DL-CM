13c463966281b4fc3f3c8977411c64f93d1d0bb3
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-23 09:44:32 +0100

    moving base model def to model module

--- dl_cm/common/models/__init__.py
+++ dl_cm/common/models/__init__.py
@@ -1,16 +1,44 @@
-from dl_cm.utils.registery import Registry
-from .base_model import BaseModel, IdModel
+from abc import ABC, abstractmethod
+
+import pydantic as pd
+import torch
+
+from dl_cm.common import DLCM
 from dl_cm.utils.ppattern.factory import BaseFactory
+from dl_cm.utils.registery import Registry
 
 MODELS_REGISTERY = Registry("Models")
 
-class ModelsFactory(BaseFactory[BaseModel]):
 
+class BaseModel(torch.nn.Module, DLCM, ABC):
     @staticmethod
-    def base_class()-> type[BaseModel]:
-        return BaseModel
-    
+    def registry() -> Registry:
+        return MODELS_REGISTERY
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @abstractmethod
+    def forward(self, x):
+        pass
+
     @classmethod
-    def default_instance(cls)-> BaseModel:
+    # @abstractmethod
+    def get_prediction_schema(cls) -> pd.BaseModel:
+        """Returns the expected prediction schema of the model"""
+        raise NotImplementedError
+
+
+class IdModel(BaseModel):
+    def forward(self, x):
+        return x
+
+
+class ModelsFactory(BaseFactory[BaseModel]):
+    @staticmethod
+    def base_class() -> type[BaseModel]:
+        return BaseModel
+
+    @classmethod
+    def default_instance(cls) -> BaseModel:
         return IdModel()
-    
