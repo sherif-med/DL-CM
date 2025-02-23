99452db84f5d91bbe1994264366ed05f96778cb5
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-23 09:40:13 +0100

    moving baselearner def to learning module

--- dl_cm/common/learning/__init__.py
+++ dl_cm/common/learning/__init__.py
@@ -1,12 +1,32 @@
-from dl_cm.utils.registery import Registry
+import torch
+
+from dl_cm.common import DLCM
+from dl_cm.common.models import BaseModel, ModelsFactory
+from dl_cm.common.typing import StepInputStruct, StepOutputStruct, namedEntitySchema
 from dl_cm.utils.ppattern.factory import BaseFactory
-from dl_cm.common.learning.base_learner import BaseLearner
+from dl_cm.utils.registery import Registry
 
 LEARNERS_REGISTERY = Registry("Learners")
 
-class LearnersFactory(BaseFactory[BaseLearner]):
 
+class BaseLearner(torch.nn.Module, DLCM):
     @staticmethod
-    def base_class()-> type[BaseLearner]:
+    def registry() -> Registry:
+        return LEARNERS_REGISTERY
+
+    def __init__(self, model: BaseModel | namedEntitySchema, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._model: BaseModel = ModelsFactory.create(model)
+
+    @property
+    def model(self):
+        return self._model
+
+    def forward(self, batch: StepInputStruct, *args, **kwargs) -> StepOutputStruct:
+        return self.model(batch["inputs"], *args, **kwargs)
+
+
+class LearnersFactory(BaseFactory[BaseLearner]):
+    @staticmethod
+    def base_class() -> type[BaseLearner]:
         return BaseLearner
-    
