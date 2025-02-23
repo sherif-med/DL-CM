1a71674901b4546d502dfff22589cfe3f73a76c3
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-23 14:48:36 +0100

    moving self registring to dlcm class and adding warped_name argument if a certain class is wrapped

--- dl_cm/common/__init__.py
+++ dl_cm/common/__init__.py
@@ -1,8 +1,15 @@
 from dl_cm import _logger as logger
-from dl_cm.utils.ppattern.self_registered_mixin import selfRegisteredMixin
+from dl_cm.utils.registery import registeredClassMixin
 
 
-class DLCM(selfRegisteredMixin):
+class DLCM(registeredClassMixin):
+    def __init_subclass__(cls, *args, wrapped_name=None, **kwargs):
+        super().__init_subclass__(*args, **kwargs)
+        if not wrapped_name:
+            wrapped_name = cls.__name__
+        if wrapped_name not in cls.registry():
+            cls.registry().register(name=wrapped_name, obj=cls)
+
     def __init__(self, *args, **kwargs):
         if kwargs:
             logger.warning(f"Unused kwargs: {kwargs} in {self.__class__.__name__}")
@@ -11,7 +18,7 @@ class DLCM(selfRegisteredMixin):
     def base_class_adapter(external_cls: type, base_cls: type) -> type:
         """Decorator to make any external class inherit from BaseClass"""
 
-        class WrappedClass(external_cls, base_cls):
+        class WrappedClass(external_cls, base_cls, wrapped_name=external_cls.__name__):
             def __init__(self, *args, **kwargs):
                 external_cls.__init__(*args, **kwargs.pop("params", {}))
                 base_cls.__init__(self, *args, **kwargs)
--- dl_cm/utils/ppattern/self_registered_mixin.py
+++
@@ -1,7 +0,0 @@
-from dl_cm.utils.registery import registeredClassMixin 
-
-class selfRegisteredMixin(registeredClassMixin):
-
-    def __init_subclass__(cls, *args, **kwargs):
-        super().__init_subclass__(*args, **kwargs)
-        cls.registry().register(cls.__name__, cls)
