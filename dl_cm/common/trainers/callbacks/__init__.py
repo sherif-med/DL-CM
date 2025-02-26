0c7cc20a14e038d2744f6e255db694a33cfef8c0
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-26 14:30:12 +0100

    remove depr validation for callback && applying new registering pattern for eternal callbacks

--- dl_cm/common/trainers/callbacks/__init__.py
+++ dl_cm/common/trainers/callbacks/__init__.py
@@ -1,41 +1,32 @@
-from dl_cm.utils.registery import Registry
+import pytorch_lightning as pl
+
 from dl_cm.common import DLCM
 from dl_cm.utils.ppattern.factory import BaseFactory
-from dl_cm.common.typing import namedEntitySchema
-import pydantic as pd
-from dl_cm.utils.ppattern.data_validation import validationMixin
-import pytorch_lightning as pl
+from dl_cm.utils.registery import Registry
 
 CALLBACKS_REGISTERY = Registry("Callbacks")
 
-class baseCallback(pl.Callback, DLCM, validationMixin):
 
-    @staticmethod
-    def config_schema() -> pd.BaseModel:
-        return namedEntitySchema
-    
+class baseCallback(pl.Callback, DLCM):
     @staticmethod
     def registry() -> Registry:
         return CALLBACKS_REGISTERY
-    
-    def __init__(self, config: dict) -> None:
-        validationMixin.__init__(self, config)
-        super().__init__()
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
 
 class CallbacksFactory(BaseFactory[baseCallback]):
-    
     @staticmethod
-    def base_class()-> type[baseCallback]:
+    def base_class() -> type[baseCallback]:
         return baseCallback
 
-from pytorch_lightning.callbacks.progress import TQDMProgressBar
-from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
-from pytorch_lightning.callbacks import LearningRateFinder
-from pytorch_lightning.callbacks import LearningRateMonitor
 
-from functools import partial
-base_callback_adapter = partial(DLCM.base_class_adapter, base_cls=baseCallback)
-CALLBACKS_REGISTERY.register(TQDMProgressBar, base_class_adapter=base_callback_adapter)
-CALLBACKS_REGISTERY.register(ModelCheckpoint, base_class_adapter=base_callback_adapter)
-CALLBACKS_REGISTERY.register(LearningRateFinder, base_class_adapter=base_callback_adapter)
-CALLBACKS_REGISTERY.register(LearningRateMonitor, base_class_adapter=base_callback_adapter)
+from pytorch_lightning.callbacks import LearningRateFinder, LearningRateMonitor
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
+
+_ = DLCM.base_class_adapter(TQDMProgressBar, base_cls=baseCallback)
+_ = DLCM.base_class_adapter(ModelCheckpoint, base_cls=baseCallback)
+_ = DLCM.base_class_adapter(LearningRateFinder, base_cls=baseCallback)
+_ = DLCM.base_class_adapter(LearningRateMonitor, base_cls=baseCallback)
