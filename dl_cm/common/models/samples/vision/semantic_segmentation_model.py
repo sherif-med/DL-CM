210eaa625651a6ae7c68a9efd45d45dfff163cdd
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-26 14:26:39 +0100

    updating semanticseg model init to use eplicit arguments

--- dl_cm/common/models/samples/vision/semantic_segmentation_model.py
+++ dl_cm/common/models/samples/vision/semantic_segmentation_model.py
@@ -1,17 +1,20 @@
-from dl_cm.common.models.base_model import BaseModel
-import segmentation_models_pytorch as smp
 import pydantic as pd
+import segmentation_models_pytorch as smp
 import torch
 
+from dl_cm.common.models import BaseModel
+
+
 class SemanticSegmentationModel(BaseModel):
-    def __init__(self, model_config:dict):
-        super().__init__(model_config)
-        self.model = smp.create_model(model_config)   
-    
+    def __init__(self, model_config: dict, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = smp.create_model(**model_config)
+
     @classmethod
     def get_prediction_schema(cls) -> pd.BaseModel:
         class PredictionSchema(pd.BaseModel):
             seg_map: torch.Tensor
+
         return PredictionSchema
 
     def forward(self, input):
