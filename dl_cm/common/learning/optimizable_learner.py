5fb86b9a91a142182f45a393aa70055511ab2092
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-02-27 13:24:00 +0100

    moving optimizer and lr_scheduler to respective learner && fixing optim type check && fixing type annot

--- dl_cm/common/learning/optimizable_learner.py
+++ dl_cm/common/learning/optimizable_learner.py
@@ -1,18 +1,17 @@
+from typing import Union
+
+from dl_cm.common import DLCM
 from dl_cm.common.learning import BaseLearner
-from dl_cm.common.tasks.optimizer import (
-    BaseLrScheduler,
-    BaseOptimizer,
-    LrSchedulerFactory,
-    OptimizerFactory,
-)
 from dl_cm.common.typing import OneOrMany, namedEntitySchema
+from dl_cm.utils.ppattern.factory import BaseFactory
+from dl_cm.utils.registery import Registry
 
 
 class OptimizableLearner(BaseLearner):
     def __init__(
         self,
-        optimizers: OneOrMany[namedEntitySchema | BaseOptimizer],
-        lr_schedulers: OneOrMany[namedEntitySchema | BaseLrScheduler],
+        optimizers: OneOrMany[Union[namedEntitySchema, "BaseOptimizer"]],
+        lr_schedulers: OneOrMany[Union[namedEntitySchema, "BaseLrScheduler"]],
         *args,
         **kwargs,
     ):
@@ -29,31 +28,80 @@ class OptimizableLearner(BaseLearner):
             self._lr_schedulers = [lr_schedulers]
 
     def init_optimizer(
-        self, optimizer_conf: namedEntitySchema | BaseOptimizer
-    ) -> BaseOptimizer:
+        self, optimizer_conf: Union[namedEntitySchema, "BaseOptimizer"]
+    ) -> "BaseOptimizer":
         if isinstance(optimizer_conf, BaseOptimizer):
             return optimizer_conf
         optimizer_conf["params"]["params"] = self.model.parameters()
         return OptimizerFactory.create(optimizer_conf)
 
     def init_lr_scheduler(
-        self, lr_scheduler_conf: namedEntitySchema | BaseLrScheduler
-    ) -> BaseLrScheduler:
+        self, lr_scheduler_conf: Union[namedEntitySchema, "BaseLrScheduler"]
+    ) -> "BaseLrScheduler":
         if isinstance(lr_scheduler_conf, BaseLrScheduler):
             return lr_scheduler_conf
         lr_scheduler_conf["params"]["optimizer"] = self.optimizers
         return LrSchedulerFactory.create(lr_scheduler_conf)
 
     @property
-    def optimizers(self) -> list[BaseOptimizer]:
+    def optimizers(self) -> list["BaseOptimizer"]:
         if all(map(lambda o: isinstance(o, BaseOptimizer), self._optimizers)):
             return self._optimizers
         self._optimizers = [self.init_optimizer(o) for o in self._optimizers]
         return self._optimizers
 
     @property
-    def lr_schedulers(self) -> list[BaseLrScheduler]:
+    def lr_schedulers(self) -> list["BaseLrScheduler"]:
         if all(map(lambda o: isinstance(o, BaseLrScheduler), self._lr_schedulers)):
             return self._lr_schedulers
         self._lr_schedulers = [self.init_lr_scheduler(o) for o in self._lr_schedulers]
         return self._lr_schedulers
+
+
+OPTIMIZER_REGiSTERY = Registry("Optimizer")
+LR_SCHEDULER_REGiSTERY = Registry("Lr scheduler")
+
+
+class BaseOptimizer(DLCM):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @staticmethod
+    def registry() -> Registry:
+        return OPTIMIZER_REGiSTERY
+
+
+class OptimizerFactory(BaseFactory[BaseOptimizer]):
+    @staticmethod
+    def base_class() -> type[BaseOptimizer]:
+        return BaseOptimizer
+
+
+class BaseLrScheduler(DLCM):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @staticmethod
+    def registry() -> Registry:
+        return LR_SCHEDULER_REGiSTERY
+
+
+class LrSchedulerFactory(BaseFactory):
+    @staticmethod
+    def base_class() -> type:
+        return BaseLrScheduler
+
+
+import torch.optim as optim
+
+for name in dir(optim):
+    attr = getattr(optim, name)
+    if isinstance(attr, type) and issubclass(attr, optim.Optimizer):
+        _ = DLCM.base_class_adapter(attr, base_cls=BaseOptimizer)
+
+import torch.optim.lr_scheduler as lr_scheduler
+
+for name in dir(lr_scheduler):
+    attr = getattr(lr_scheduler, name)
+    if isinstance(attr, type) and issubclass(attr, lr_scheduler._LRScheduler):
+        _ = DLCM.base_class_adapter(attr, base_cls=BaseLrScheduler)
