9469264027eea01df4ac6bc3f203a5bea4495d80
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-03-25 05:56:41 +0100

    removing preprocessing base class and using general transformation instead as it serves the same purpose && parent_inde method for preprocessed dataset

--- dl_cm/common/data/datasets/preprocessed_dataset.py
+++ dl_cm/common/data/datasets/preprocessed_dataset.py
@@ -1,35 +1,13 @@
-from typing import Callable, Type
+from typing import Callable
 
 from dl_cm.common.data.datasets import (
     COMPOSED_DATASET_CLASS,
     TOP_DATASET_CLASS,
     CompositionDataset,
 )
-from dl_cm.utils.ppattern.factory import BaseFactory
-from dl_cm.utils.registery import Registry
-
-PREPROCESSING_REGISTERY = Registry("Preprocessing")
-
-
-class BasePreprocessing(DLCM):
-    @staticmethod
-    def registry() -> Registry:
-        return PREPROCESSING_REGISTERY
-
-
-class PreprocessingFactory(BaseFactory[BasePreprocessing]):
-    @staticmethod
-    def base_class() -> Type["DLCM"]:
-        return BasePreprocessing
-
-
-@PREPROCESSING_REGISTERY.register(name="id")
-class PreprocessingId(BasePreprocessing):
-    def __init__(self):
-        pass
-
-    def __call__(self, item):
-        return item
+from dl_cm.common.data.transformations.general_transformation import (
+    GeneralTransformationFactory,
+)
 
 
 class PreprocessedDataset(
@@ -37,10 +15,15 @@ class PreprocessedDataset(
 ):
     def __init__(self, preprocessing_fn: str | Callable = "id", *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.preprocessing_callable = PreprocessingFactory.create(preprocessing_fn)
+        self.preprocessing_callable = GeneralTransformationFactory.create(
+            preprocessing_fn
+        )
 
     def __len__(self):
         return len(self.parent_dataset)
 
     def __getitem__(self, index):
         return self.preprocessing_callable(self.parent_dataset[index])
+
+    def parent_index(self, index):
+        return index
