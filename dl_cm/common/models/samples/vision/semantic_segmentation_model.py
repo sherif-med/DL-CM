acac19a0a42880542cc151187eb601ef3f51b3bf
sherif-med <cherif.abedrazek@gmail.com>
2025-09-06 05:07:41 +0000

    semantic seg model returns label and preds maps && takes output keys as well

--- dl_cm/common/models/samples/vision/semantic_segmentation_model.py
+++ dl_cm/common/models/samples/vision/semantic_segmentation_model.py
@@ -6,10 +6,13 @@ from dl_cm.common.models import BaseModel
 
 
 class SemanticSegmentationModel(BaseModel):
-    def __init__(self, input_key, *args, **kwargs):
+    def __init__(self, input_key, seg_output_key="seg_map", label_output_key="label_map", *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model = smp.create_model(*args, **kwargs)
         self.input_key = input_key
+        self.seg_output_key = seg_output_key
+        self.label_output_key = label_output_key
+
 
     @classmethod
     def get_prediction_schema(cls) -> pd.BaseModel:
@@ -20,4 +23,5 @@ class SemanticSegmentationModel(BaseModel):
 
     def forward(self, input):
         seg_map = self.model(input[self.input_key])
-        return {"seg_map": seg_map}
+        label_map = seg_map.argmax(dim=1)
+        return {self.seg_output_key: seg_map, self.label_output_key:label_map}
