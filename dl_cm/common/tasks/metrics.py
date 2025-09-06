d57f724a1f9ac7a43f84f936c219f1edd86b2052
sherif-med <cherif.abedrazek@gmail.com>
2025-09-06 05:09:03 +0000

    fixing metric wrapper & adding namedmixin for base metric

--- dl_cm/common/tasks/metrics.py
+++ dl_cm/common/tasks/metrics.py
@@ -3,33 +3,52 @@ from torchmetrics.metric import Metric
 
 from dl_cm.common import DLCM
 from dl_cm.utils.ppattern.factory import BaseFactory
+from dl_cm.utils.ppattern.named_mixin import NamedInstanceMixin
 from dl_cm.utils.registery import Registry
 
 METRICS_REGISTRY = Registry("Metrics")
 
 
 def base_metric_adapter(metric_cls: type[Metric]):
-    """Decorator to make any torchmetrics.Metric work with specific dictionary inputs keys"""
+    """Wrap a torchmetrics.Metric (even factory ones like F1Score) to work with dict inputs."""
 
-    class WrappedMetric(metric_cls, BaseMetric):
-        def __init__(self, *args, **kwargs):
-            """Wraps a metric to extract values from dictionary inputs."""
-            super().__init__(*args, **kwargs.pop("params", {}))
-            BaseMetric.__init__(self, *args, **kwargs)
+    def wrapper(*args, **kwargs):
+        # Step 1: Instantiate the metric (handles F1Score.__new__)
+        preds_key = kwargs.pop("preds_key", None)
+        target_key = kwargs.pop("target_key", None)
+        metric_instance = metric_cls(*args, **kwargs)
 
-        def update(self, preds: dict, target: dict = None):
-            """Extract tensors and call the original update method."""
-            pred_tensor = preds[self.preds_key] if self.preds_key else preds
-            if target is None:
-                super().update(pred_tensor)
-                return
-            target_tensor = target[self.target_key] if self.target_key else target
-            super().update(pred_tensor, target_tensor)
+        # Step 2: Create a dynamic subclass of the *instance’s class*
+        class WrappedMetric(metric_instance.__class__, BaseMetric):
 
-    return WrappedMetric
+            def instance_name(self):
+                if self._instance_count[metric_instance.__class__] == 0:
+                    return metric_instance.__class__.__name__
+                else:
+                    self._instance_count[metric_instance.__class__] += 1
+                    return (
+                        f"{metric_instance.__class__.__name__}_{self._instance_count[metric_instance.__class__] - 1}"
+                    )
+
+            def update(self, preds: dict, target: dict = None):
+                pred_tensor = preds[self.preds_key] if self.preds_key else preds
+                if target is None:
+                    super().update(pred_tensor)
+                    return
+                target_tensor = target[self.target_key] if self.target_key else target
+                super().update(pred_tensor, target_tensor)
+
+        # Step 3: Create a new instance WITHOUT re-calling __init__
+        wrapped = WrappedMetric.__new__(WrappedMetric)
+        wrapped.__dict__ = metric_instance.__dict__.copy()  # copy state
+        BaseMetric.__init__(wrapped, preds_key, target_key)  # initialize BaseMetric separately
+
+        return wrapped
+
+    return wrapper
 
 
-class BaseMetric(DLCM):
+class BaseMetric(NamedInstanceMixin, DLCM):
     @staticmethod
     def registry() -> Registry:
         return METRICS_REGISTRY
