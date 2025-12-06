a4bd6740dbcb5a6666901eab7c0b41aa61e34069
sherif-med <cherif.abedrazek@gmail.com>
2025-12-06 16:41:59 +0100

    adding sampler_adapter  && fixing registring

--- dl_cm/common/data/samplers/__init__.py
+++ dl_cm/common/data/samplers/__init__.py
@@ -1,5 +1,6 @@
 import torch
 import torch.utils.data.sampler
+from pytorch_lightning.overrides.distributed import _IndexBatchSamplerWrapper
 
 from dl_cm.common import DLCM
 from dl_cm.utils.ppattern.factory import BaseFactory
@@ -15,6 +16,24 @@ class BaseSampler(DLCM):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+    
+    param_factory_map: dict[str, BaseFactory] = {
+        #"sampler": SamplersFactory,
+    }
+    
+    @classmethod
+    def instantiate_params(cls, params: dict) -> dict:
+        """
+        Instantiate all parameters that are factory-creatable
+
+        Goes through all parameters and if a parameter name is in the
+        param_factory_map, it uses the respective factory to instantiate the
+        parameter.
+        """
+        for k, respective_factory in cls.param_factory_map.items():
+            if k in params:
+                params[k] = respective_factory.create(params.get(k))
+        return params
 
 
 class SamplersFactory(BaseFactory[BaseSampler]):
@@ -24,9 +43,24 @@ class SamplersFactory(BaseFactory[BaseSampler]):
             return (BaseSampler, torch.utils.data.Sampler, _IndexBatchSamplerWrapper)
         return BaseSampler
 
+def base_sampler_adapter(sampler_cls: type):
+    """Decorator to make any external sampler inherit from BaseSampler
+    """
+
+    class WrappedSampler(sampler_cls, BaseSampler):
+        def __init__(self: BaseSampler, *args, **kwargs):
+            """Wraps a sampler to pop unused arguments and instantiate params."""
+            BaseSampler.__init__(self)
+            # instantiate object params
+            kwargs = self.instantiate_params(kwargs)
+            sampler_cls.__init__(self, **kwargs)
+
+    return WrappedSampler
 
 # Register all samplers
 for name in dir(torch.utils.data.sampler):
-    cls = getattr(torch.utils.data.sampler, name)
-    if isinstance(cls, type) and issubclass(cls, torch.utils.data.Sampler):
-        _ = DLCM.base_class_adapter(cls, base_cls=BaseSampler)
+    attr = getattr(torch.utils.data.sampler, name)
+    if isinstance(attr, type) and issubclass(attr, torch.utils.data.Sampler):
+        SAMPLER_REGISTRY.register(
+            obj=attr, name=name, base_class_adapter=base_sampler_adapter
+        )
