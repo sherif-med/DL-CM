2f84ceea4e88318c68b5f712b6528a920af40bb1
sherif-med <cherif.abedrazek@gmail.com>
2025-12-06 16:22:23 +0100

    adding default write interval && fixing typo of dict key && (require more corrections)

--- dl_cm/common/trainer/callbacks/prediction_writer.py
+++ dl_cm/common/trainer/callbacks/prediction_writer.py
@@ -1,18 +1,18 @@
 from pathlib import Path
 from skimage.io import imsave
 from pytorch_lightning.callbacks import BasePredictionWriter, Callback
-import numpy as np
+from dl_cm.common.trainer.callbacks import baseCallback
 import os
 
 
-class ImagesPredictionWriter(BasePredictionWriter):
+class ImagesPredictionWriter(BasePredictionWriter, baseCallback):
 
     #@rank_zero_only
     def __init__(
         self,
         output_dir,
-        write_interval,
-        predicted_map_key
+        predicted_map_key,
+        write_interval="batch",
     ):
         super().__init__(write_interval)
         self.output_dir = output_dir
@@ -29,12 +29,13 @@ class ImagesPredictionWriter(BasePredictionWriter):
         batch_idx,
         dataloader_idx,
     ):
-        preds, filenames = prediction["preds"][self.predicted_map_key], batch["id"]
-        preds = preds.cpu().numpy().astype('uint8')
+        # TODO: Treat more edge cases (this is very weak impl)
+        preds, filenames = prediction["predictions"][self.predicted_map_key], batch["inputs"]["id"]
+        preds = preds.cpu().numpy().astype('uint8') * 255
 
         for prediction, filename in zip(preds, filenames):
             output_file = Path(self.output_dir, os.path.basename(filename)).with_suffix(".png")
-            imsave(output_file, prediction[0], check_contrast=False)
+            imsave(output_file, prediction, check_contrast=False)
     
     def on_predict_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
         if not self.interval.on_batch:
