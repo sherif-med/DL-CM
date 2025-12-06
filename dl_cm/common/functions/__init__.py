e1faaed103d7537772243ae728de73fc6f9220f0
sherif-med <cherif.abedrazek@gmail.com>
2025-12-06 16:46:22 +0100

    adding base callable

--- dl_cm/common/functions/__init__.py
+++ dl_cm/common/functions/__init__.py
@@ -1,12 +1,18 @@
 from collections.abc import Callable
 
+from dl_cm.common import DLCM
 from dl_cm.utils.ppattern.factory import BaseFactory
 from dl_cm.utils.registery import Registry
 
 FUNCTIONS_REGISTERY = Registry("Functions")
 
+class BaseCallable(DLCM):
+    @staticmethod
+    def registry() -> Registry:
+        return FUNCTIONS_REGISTERY
+
 
 class FunctionsFactory(BaseFactory[Callable]):
     @staticmethod
     def base_class(similar=False) -> type[BaseCallable]:
-        return Callable
+        return BaseCallable
