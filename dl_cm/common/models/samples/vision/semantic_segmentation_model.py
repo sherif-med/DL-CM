9ec65fd7932f71bc6966cbfb2ce303c3966f7d21
sherif-med <cherif.abedrazek@gmail.com>
2025-12-06 16:11:33 +0100

    updating semanticSegModel to support both binary and multiclass label maps

--- dl_cm/common/models/samples/vision/semantic_segmentation_model.py
+++ dl_cm/common/models/samples/vision/semantic_segmentation_model.py
@@ -23,5 +23,10 @@ class SemanticSegmentationModel(BaseModel):
 
     def forward(self, input):
         seg_map = self.model(input[self.input_key])
-        label_map = torch.softmax(seg_map,1).argmax(dim=1)
+        if seg_map.shape[1] == 1:  # Binary segmentation (1 channel)
+            # Apply sigmoid for binary
+            label_map = (torch.sigmoid(seg_map) > 0.5).long().squeeze(1)
+        else:  # Multiclass segmentation (n channels)
+            # Apply softmax and argmax for multiclass
+            label_map = torch.softmax(seg_map, dim=1).argmax(dim=1)
         return {self.seg_output_key: seg_map, self.label_output_key:label_map}
